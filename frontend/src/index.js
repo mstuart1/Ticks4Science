172d2c23ce40224f9a1f50d4872db62387f21c97
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-02-21 15:54:50 -0500

    add basepage/homepage to package.json and index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,12 @@ import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 
 const rootElement = document.getElementById('root')
-// const basename = process.env.REACT_APP_BASENAME || '/basename-not-working'
+const basename = process.env.REACT_APP_BASENAME || '/basename-not-working'
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter 
-        // basename={basename}
+        basename={basename}
         >
         <App/>
         </BrowserRouter>
