c06fb6494bd88b2cb66e8268bfba62b87fe9817b
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-02-21 15:55:14 -0500

    make tickname italics and add link to submit a tick

--- frontend/src/components/ticks/TickPage.jsx
+++ frontend/src/components/ticks/TickPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { PageContainer, ScreenContainer } from '../about/About.styled'
 import { MenuButton } from '../appHeader/AppHeader.styled'
+import { TickName } from './TickPage.styled'
 
 const TickPage = () => {
   let {sciName} = useParams()
@@ -17,7 +18,7 @@ const TickPage = () => {
    <ScreenContainer>
      <PageContainer>
        <div>
-       <h1 style={{textAlign: 'center'}}>This page will contain information about {sciName}.</h1>
+       <h1 style={{textAlign: 'center'}}>This page will contain information about <TickName>{sciName}</TickName>.</h1>
 
        <h2>Text</h2>
        <p>
@@ -27,8 +28,8 @@ const TickPage = () => {
        <p>
        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
        </p>
-       <MenuButton onClick={handleToggle}>Show Me Photos of Ticks!</MenuButton>
-       {togglePhotos && photoList.length > 0 ? photoList : <h3>Sorry, no photos for this tick have been submitted</h3>}
+       <MenuButton style={{color: 'black'}}onClick={handleToggle}>Show Me Photos of Ticks</MenuButton>
+       {togglePhotos && photoList.length > 0 ? photoList : <h3>Sorry, no photos for this tick have been submitted, please <Link to='/submit'>submit a tick</Link> to help us share information.</h3>}
        </div>
      </PageContainer>
    </ScreenContainer>
---
+++ frontend/src/components/ticks/TickPage.styled.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const TickName = styled.span`
+font-style: italic;
+`
