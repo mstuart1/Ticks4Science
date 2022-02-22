536acc81a67c3e5eb1357e3fae959187afd08a17
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-02-22 14:14:27 -0500

    add scroll to top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import About from './components/about/About';
 import AppHeader from './components/appHeader/AppHeader';
 import SubmitPhoto from './components/photos/SubmitPhoto';
+import ScrollToTop from './components/ScrollToTop';
 import TickPage from './components/ticks/TickPage';
 // import RutgersFooter from './components/rutgersFooter/RutgersFooter';
 // import RutgersHeader from './components/rutgersHeader/RutgersHeader';
@@ -12,11 +13,13 @@ const App = () => {
   <div>
     {/* <RutgersHeader/> */}
     <AppHeader/>
+    <ScrollToTop>
       <Routes>
         <Route path='/' element={<About/>}/>
         <Route path='/submit' element={<SubmitPhoto/>}/>
         <Route path='/tick/:sciName' element={<TickPage/>}/>
       </Routes>
+      </ScrollToTop>
       
       {/* <RutgersFooter/> */}
   </div>
