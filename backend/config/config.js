055b9add61e22c39d1391cc56603ed86eafe0dd9
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-14 10:16:37 -0400

    create config.js

---
+++ backend/config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      charset: "utf8mb4",
+    },
+    define: {
+      freezeTableName: true,
+      // paranoid: true,
+    },
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory",
+  },
+  production: {
+    dialect: "sqlite",
+    storage: "./wpf.sqlite3",
+  },
+};
