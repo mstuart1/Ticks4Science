75033f2bf1f5df2a18760edf8981f46b0f11f52a
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-15 16:42:51 -0400

    create submission post

--- backend/api/submission.controller.js
+++ backend/api/submission.controller.js
@@ -5,8 +5,26 @@ const Subm = db.submission;
 exports.createSubm = async (req, res, next) => {
     console.log(`@@@@---receiving form from arcgis---@@@@`);
     try {
-        console.log(req.body)
-        return res.json({status: "success", newContactId: newContact.id})
+        // console.log(JSON.stringify(req.body, null, 1))
+        // create an array of photo urls for all the attachments
+        let front, back, createdRecord;
+        let incomingInfo = req.body;
+        
+        let photos = incomingInfo.feature.attachments.submitPhoto.map(photo => photo.url)
+        // if there are more than 2 ???
+        if (photos.length > 0){
+          front = photos[0];
+          back = photos[1];
+        }
+        incomingInfo.photoFrontUrl = front;
+        incomingInfo.photoBackUrl = back;
+        
+        await db.sequelize
+      .transaction(async (t) => {
+        createdRecord = await Subm.create(incomingInfo, {transaction: t})
+      });
+      console.log(JSON.stringify(createdRecord, null, 1))
+        return res.json({data: createdRecord})
     } catch (err) {
         next({status: 400, message: err.message})
     }
