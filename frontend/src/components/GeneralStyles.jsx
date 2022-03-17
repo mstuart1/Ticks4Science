818b02c20f6ed798a0cf2a4b3a2bf4a208fa7bcc
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-17 09:59:49 -0400

    add boxInset to general styles

--- frontend/src/components/GeneralStyles.jsx
+++ frontend/src/components/GeneralStyles.jsx
@@ -1,17 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ScreenContainer = styled.div`
-background-color: white;
-font-family: 'Source Sans Pro', sans-serif;
-display: flex;
-justify-content: center;
-`
+  background-color: white;
+  font-family: 'Source Sans Pro', sans-serif;
+  display: flex;
+  justify-content: center;
+`;
 export const PageContainer = styled.div`
-width: 1366px;
-display: flex;
-justify-content: center;
-`
+  width: 1366px;
+  display: flex;
+  justify-content: center;
+`;
 export const TextDiv = styled.div`
-width: 800px;
-font-size: 1.25em;
-`
+  width: 800px;
+  font-size: 1.25em;
+`;
+export const BoxInset = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5em;
+  align-items: center;
+  border-radius: 0.5em;
+`;
