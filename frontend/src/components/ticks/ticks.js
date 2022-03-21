fd4716b3e4f0ca37112c83b734e802ebd3ab3fd3
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-21 11:50:11 -0400

    add more detail to D. variabilis

--- frontend/src/components/ticks/ticks.js
+++ frontend/src/components/ticks/ticks.js
@@ -1,9 +1,18 @@
 export const ticks = [
-    {id: 1, title: 'Amblyomma americanum', path: `/ticks/1` },
+    {id: 1, title: 'Amblyomma americanum',
+     path: `/ticks/1` },
     {id: 2, title: 'Amblyomma maculatum', path: `/ticks/2` },
     {id: 3, title: 'Carios kelleyi', path: `/ticks/3` },
     {id: 4, title: 'Dermacenter albipictus', path: `/ticks/4` },
-    {id: 5, title: 'Dermacenter variabilis', path: `/ticks/5` },
+    {id: 5, title: 'Dermacenter variabilis', path: `/ticks/5`, common: 'American Dog Tick, Wood Tick', first: 'Ocean County', second: 'Burlington County', comment: 'Understood to be statewide', loc: 'Rural and suburban areas, and was known to be found in vacant lots and rights-of-ways', habitat: 'Tree-line edges with barely any tree coverage, grassy fields, or where vegetation is not maintained', info: 'The American Dog Tick, also known as the Wood Tick is commonly found in overgrown fields rather than in wooded, forestry areas. Pasteurizing cattle and horses are found to host these ticks, along with other types of mammals that roam fields including white-footed mice, meadow voles, and rats.', stages: [
+        {stage: 'larvae', photos: [], active: 'May to Sep', feed: 'small wild mammals'},
+        {stage: 'nymphs', photos: [], active: 'Apr to Jul', feed: 'small wild mammals'},
+        {stage: 'adults', photos: [], active: 'Mar to Aug', feed: 'large mammals such as wild mammals, cattle, pets, and people'},
+    ], sexDiff: [
+        {sex: 'Female', desc: 'white markings on the back'},
+        {sex: 'Male', desc: 'white markings on entire body'},
+        {sex: 'Nymph', desc: 'dark brown'},
+    ], shape: 'oblong', engorgedPhotos: [] }, 
     {id: 6, title: 'Haemaphysalis leporispalustris', path: `/ticks/6` },
     {id: 7, title: 'Haemaphysalis longicornis', path: `/ticks/7` },
     {id: 8, title: 'Ixodes brunneus', path: `/ticks/8` },
