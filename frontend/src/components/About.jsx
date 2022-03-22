ec9514ab7426a0a521eff3df06c053a36ab5983c
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-22 12:35:24 -0400

    add report to About

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { PageContainer, ScreenContainer, StyledButton, TextDiv } from "./GeneralStyles";
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
     <ScreenContainer>
       <PageContainer>
         <TextDiv>
-          <h1 style={{textAlign: "center"}}>Welcome to NJ Ticks 4 Science!</h1>A citizen science project
+          <h1 style={{textAlign: "center"}}>Welcome to NJ Ticks 4 Science!</h1>
+          <p>A citizen science project
           where we are enlisting the people in New Jersey to submit ticks they
           come across in their everyday lives! The goal is to understand the
           statewide risk of tick-borne pathogens to NJ communities. With your
@@ -14,7 +16,14 @@ const About = () => {
           pathogens in NJ and their specific locations. To begin a tick
           submission, utilize the tabs to the right of the screen. We encourage
           you to follow the steps on how to send a tick pic to begin the tick
-          submission process!
+          submission process!</p>
+          
+            <h2 style={{textAlign: "center"}}>View a report of the 11 ticks found throughout New Jersey.</h2>
+          <p>
+            <a href='https://vectorbio.rutgers.edu/outreach/Occi2019TickListNJ.pdf'>Annotated List of the Hard Ticks (Acari: Ixodida: Ixodidae) of New Jersey</a><br/>
+            Since this report, two more ticks have been discovered in NJ, which we review within <Link to='/notReady'>Ticks of NJ</Link>
+          </p>
+          
           <a href='mailto:cvb@rutgers.edu' target='_blank'><StyledButton color='black'>Contact Us</StyledButton></a>
         </TextDiv>
       </PageContainer>
