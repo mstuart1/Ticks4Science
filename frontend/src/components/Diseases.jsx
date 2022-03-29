49816ddecea5005b20badd60e88b1fb705754744
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-29 16:06:08 -0400

    add more diseases

--- frontend/src/components/Diseases.jsx
+++ frontend/src/components/Diseases.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { ticks } from './ticks/ticks'
+import { emergingDiseases, commonDiseases } from './diseaseCards'
+
 const RemovalPage = {
     Container: styled.div`
     display: flex;
@@ -33,7 +36,7 @@ const RemovalPage = {
     justify-content: center;
     border: 1px solid black;
     border-radius: 1em;
-    background-color: ${({theme}) => theme.colors.greyBG};
+    background-color: ${({ theme }) => theme.colors.greyBG};
     margin: 1em 0;
     span {
         padding: 0 1em;
@@ -44,35 +47,65 @@ const RemovalPage = {
 
 const Diseases = () => {
 
+    const createDiseaseCards = (diseaseArray) => {
+        // TODO make this handle any number of associated ticks
+        let elements = diseaseArray.map((dis, i) => {
+            
+            let assocElement;
+            let tickInfo = ticks.filter(tick => dis.assoc.includes(tick.id));
+            if (tickInfo.length > 1) {
+                assocElement = (<span>Associated with the <Link to={`/ticks/${tickInfo[0].id}`}>{tickInfo[0].common}</Link> and <Link to={`/ticks/${tickInfo[1].id}`}>{tickInfo[1].common}</Link></span>)
+            } else {
+                assocElement = (<span>Associated with the <Link to={`/ticks/${tickInfo[0].id}`}>{tickInfo[0].common}</Link></span>)
+            }
+
+            return  (
+                <RemovalPage.Card key={i}>
+                    <span>{dis.title}</span>
+                    <br />
+                    <span>{dis.symptoms}</span>
+                    <br />
+                    {assocElement}
+                </RemovalPage.Card>)
+
+
+        })
+        return elements
+    }
+
+    let commonElements = createDiseaseCards(commonDiseases)
+    let emergingElements = createDiseaseCards(emergingDiseases)
+
+console.log(commonElements)
+
     return (
         <RemovalPage.Container>
             <RemovalPage.Title>
                 Tick-borne Diseases
             </RemovalPage.Title>
             <RemovalPage.Para>
+                Diseases carried by ticks that are infected with a bacteria, virus, or parasite are called tick-borne diseases. When a tick bites a person or pet, there is a potential that an infection can occur depending on which tick and if that tick was carrying a tick-borne disease. It’s important to recognize the signs and symptoms of common tick-borne diseases in New Jersey and seek medical attention immediately after a tick bite if you suspect an infection.
+            </RemovalPage.Para>
+            <RemovalPage.Para>
+                The common tick-borne diseases in New Jersey include anaplasmosis, babesiosis, ehrlichiosis, Lyme Disease, and Rocky Mountain Spotted Fever.
+            </RemovalPage.Para>
+            <RemovalPage.CardContainer>
+                {commonElements}
+
+            </RemovalPage.CardContainer>
+
+
+            <RemovalPage.Para>
                 With the growing population of ticks in New Jersey, there may also be growing tick-borne diseases that are currently undetected in the state. It's important to identify ticks and potential pathogens you may come across in New Jersey, and that's why we encourage you to <Link to='/preSurvey'>Submit</Link> any and all ticks to NJ Ticks 4 Science! While this page highlights the common tick-borne diseases and emerging tick-borne diseases in New Jersey, there are rising cases in the United States. As citizen scientists, becoming educated on your surroundings and pathogens will keep you and your loved ones safe when enjoying the outdoors!
             </RemovalPage.Para>
             <div>Emerging New Jersey Tick-borne Diseases
             </div>
             <RemovalPage.CardContainer>
-                <RemovalPage.Card>
-                    <span>Powassan Virus</span>
-                    <br />
-                    <span>Symptoms may begin 1 week to 1 month after initial tick bite.</span>
-                    <br />
-                    <span>Associated with the <Link to='/ticks/11'>Blacklegged tick</Link> and <Link to='/ticks/9'>Groundhog tick</Link></span>
-                </RemovalPage.Card>
-                <RemovalPage.Card>
-                    <span>Southern tick-associated rash illness (STAR)</span>
-                    <br />
-                    <span>Symptoms may appear 7 day after initial tick bite.</span>
-                    <br />
-                    <span>Associated with the <Link to='/ticks/1'>Lone star tick</Link>.</span>
-                </RemovalPage.Card>
+                {emergingElements}
 
             </RemovalPage.CardContainer>
             <RemovalPage.Para>
-            For a further list of the tick-borne diseases in the United States, and a specific descriptions of each disease, visit the Centers for Disease Control and Prevention website on <a href='https://www.cdc.gov/ticks/diseases/index.html'>Diseases Transmitted by Ticks</a>.
+                For a further list of the tick-borne diseases in the United States, and a specific descriptions of each disease, visit the Centers for Disease Control and Prevention website on <a href='https://www.cdc.gov/ticks/diseases/index.html'>Diseases Transmitted by Ticks</a>.
 
             </RemovalPage.Para>
 
---
+++ frontend/src/components/diseaseCards.js
@@ -0,0 +1,40 @@
+export const emergingDiseases = [
+    {
+        title: 'Powassan Virus',
+        symptoms: 'Symptoms may begin 1 week to 1 month after initial tick bite.',
+        assoc: [11, 9]
+    },
+    {
+        title: 'Southern tick-associated rash illness (STARI)',
+        symptoms: 'Symptoms may appear 7 day after initial tick bite',
+        assoc: [1]
+    },
+]
+
+export const commonDiseases = [
+    {
+        title: 'Anaplasmosis',
+        symptoms: 'Symptoms may begin 1-2 weeks after initial tick bite',
+        assoc: [11]
+    },
+    {
+        title: 'Babesiosis',
+        symptoms: '',
+        assoc: [11]
+    },
+    {
+        title: 'Ehrlichiosis',
+        symptoms: 'Symptoms may begin 1-2 weeks after initial tick bite',
+        assoc: [1]
+    },
+    {
+        title: 'Lyme Disease',
+        symptoms: 'Symptoms may begin 3 to 30 days after initial tick bite',
+        assoc: [11]
+    },
+    {
+        title: 'Spotted Fever Group Rickettsioses (SFGR)',
+        symptoms: '',
+        assoc: [5,13]
+    },
+]
