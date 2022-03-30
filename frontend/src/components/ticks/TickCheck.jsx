c27f94bd06d8d9f7ddcbb20d49daf80dadc57e26
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-03-30 10:25:00 -0400

    use General Styles instead of local

--- frontend/src/components/ticks/TickCheck.jsx
+++ frontend/src/components/ticks/TickCheck.jsx
@@ -1,39 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-
-const RemovalPage = {
-Container: styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-font-size: 1.1em;
-padding: 2em;
-
-li {
-    padding: 1em 0;
-}
-`,
-Title: styled.h2`
-font-size: 3rem;
-font-weight: 700;
-`,
-Para: styled.p`
-padding: 1rem;
-display: flex;
-flex-direction: column;
-align-items: center;
-
-`
-
-}
-
+import { BasicPage } from '../GeneralStyles'
 
 const TickCheck = ()=> {
     return (
-        <RemovalPage.Container>
-            <RemovalPage.Title>Tick Checks</RemovalPage.Title>
-            <RemovalPage.Para>The most effective way to know if you have a tick is to check yourself and your pets after spending time outside. Practicing thorough tick checks is essential because ticks can take anywhere between 15 minutes to several hours to find a spot they like. There are three levels of checks to perform given how deceptive ticks can be.</RemovalPage.Para>
+        <BasicPage.Text>
+            <BasicPage.Title>Tick Checks</BasicPage.Title>
+            <p>The most effective way to know if you have a tick is to check yourself and your pets after spending time outside. Practicing thorough tick checks is essential because ticks can take anywhere between 15 minutes to several hours to find a spot they like. There are three levels of checks to perform given how deceptive ticks can be.</p>
             <ol>
                 <li>
                 Inspection #1 - Clothing<br/>
@@ -52,20 +25,20 @@ When you're taking a shower, this will give you another opportunity to thoroughl
                 </li>
                
             </ol>
-            <RemovalPage.Para>
+            <p>
             <span>Don't forget about your pet!</span><br/>
 	Checking your pet and even giving them a rinse is important 
 because some ticks may attach to them. Regularly check your dog or outdoor cat even if your pet is on a preventative. Inspection sites can include under the collar, under the tail, inside the groin area, between the toes, under the front legs, and elbows.
 
-            </RemovalPage.Para>
-            <RemovalPage.Para>
+            </p>
+            <p>
                 To learn more, check out <Link to='/removal'>Tick Removal</Link>
-            </RemovalPage.Para>
-            <RemovalPage.Para>
+            </p>
+            <p>
             Even though we can check ourselves and our pets for ticks after adventures, we should consider taking necessary steps toward personal and pet <Link to='/prevention'>Tick Prevention</Link> to ensure our safety from common <Link to='/disease'>Tick-borne Illnesses</Link> in New Jersey.
-            </RemovalPage.Para>
+            </p>
            
-        </RemovalPage.Container>
+        </BasicPage.Text>
     )
 }
 
