ae39c7f03cb789fd10093290d862e0d033103f75
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-05 08:16:21 -0400

    create html for Horizontal Scroller

---
+++ frontend/src/components/HorizontalScroller.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// whitespace method
+
+
+const HorizontalScroller = () => {
+  return (
+    <div class="scrolling-wrapper">
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+</div>
+  )
+}
+
+export default HorizontalScroller
