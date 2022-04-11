68cf24978fd3045eb9e47fe256c7a3630e0024f1
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-11 14:36:21 -0400

    add uploadPhoto controller

--- backend/api/submission.controller.js
+++ backend/api/submission.controller.js
@@ -1,11 +1,12 @@
 const db = require("../models");
+const Resize = require("../resize");
 // const Op = db.Sequelize.Op;
 const Subm = db.submission;
 
 // TODO problem with getting photos - wants a token in order to view photo, so have to be logged in to rutgers.maps - either API needs to be able to download the photo somewhere or Dina's group needs to be able to access the Survey123 website so they can see the data (ideal)
 
 exports.createSubm = async (req, res, next) => {
-    console.log(`@@@@---receiving form from arcgis---@@@@`);
+    console.log(`@@@@---receiving form from arcgis or website---@@@@`);
     console.log(JSON.stringify(req.body, null, 1))
     try {
         // console.log(JSON.stringify(req.body, null, 1))
@@ -31,6 +32,25 @@ exports.createSubm = async (req, res, next) => {
     } catch (err) {
       console.log(err.message)
         next({status: 400, message: err.message})
+    }
+  
+}
+exports.uploadPhoto = async (req, res, next) => {
+  // TODO need to return the storage path so that can send it the db with the rest of the form info
+    console.log(`@@@@---receiving image from website---@@@@`);
+    
+    try {
+        const imagePath = path.join(__dirname, '/tickImages');
+        const fileUpload = new Resize(imagePath)
+        if (!req.file) {
+          throw new Error('Please provide an image')
+        }
+        console.log(req.file)
+        const filename = await fileUpload.save(req.file.buffer);
+        return res.json({name: filename, path: fileUpload})
+    } catch (err) {
+      console.log(err.message)
+        next(err)
     }
   
 }
