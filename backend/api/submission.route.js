fea81c0d3f454d1c856119423bf7c99d1eebca6b
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-11 14:19:55 -0400

    create upload route

--- backend/api/submission.route.js
+++ backend/api/submission.route.js
@@ -1,9 +1,11 @@
 module.exports = (app) => {
     const SubmCtl  = require("./submission.controller");
     const router = require("express").Router();
+    const upload = require('./uploadMiddleware');
   
     router
       .post("/", SubmCtl.createSubm)
+      .post('/images', upload.single('image'), SubmCtl.uploadPhoto)
 
       app.use("/api/submission", router);
   };
