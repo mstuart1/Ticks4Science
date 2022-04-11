817d0fe5556703cc3a8be67625c15905567a4e45
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-11 11:48:41 -0400

    remove styles to GeneralStyles file

--- frontend/src/components/ticks/TickCheckMenu.jsx
+++ frontend/src/components/ticks/TickCheckMenu.jsx
@@ -1,23 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { BasicPage } from '../GeneralStyles'
-import styled from 'styled-components'
-
-const MenuStyles = {
-    LinkSpec: styled(Link)`
-    text-decoration: none;
-    color: ${({theme}) => theme.colors.rutgersTeal};
-    `,
-    CardSpecial: styled.div`
-    
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  width: 22.5rem;
-  height: 5rem;
-  `,
-}
 
 let menuArray = [
     {
@@ -29,15 +11,15 @@ let menuArray = [
     {id: 3, title:'Tick Prevention',path: '/prevention'},
 ]
 let menuElements = menuArray.map(item => (
-    <MenuStyles.LinkSpec key={item.id} to={item.path}>
+    <BasicPage.LinkButton.LinkSpec key={item.id} to={item.path}>
 <BasicPage.HoverCard>
-<MenuStyles.CardSpecial>
+<BasicPage.LinkButton.CardSpecial>
                 <span> {item.title}</span>
                
-              </MenuStyles.CardSpecial>
+              </BasicPage.LinkButton.CardSpecial>
                
             </BasicPage.HoverCard>
-    </MenuStyles.LinkSpec>
+    </BasicPage.LinkButton.LinkSpec>
 ))
 
 const TickCheckMenu = () => {
