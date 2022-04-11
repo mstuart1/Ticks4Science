4337fad17a1ddd34e342cc672836af15f057d1b9
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-11 11:01:42 -0400

    update the About page based on updated slide2

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BasicPage } from "./GeneralStyles";
 import TickReportLink from "./ticks/TickReportLink";
+import tickCircle from '../images/Image017.jpg'
 
 
 
@@ -18,15 +19,26 @@ const About = () => {
       The goal is to understand the statewide risk of tick-borne pathogens to NJ communities. With your help, we can learn about the growing tick species and tick-borne pathogens in NJ and their specific locations.
 
       </p><p>
-      To begin a tick submission, we encourage you to follow the <BasicPage.InnieLink to='/steps'><span>Steps to Participate</span></BasicPage.InnieLink> on how to send a tick pic to begin the tick submission process!
+      To begin a tick submission, we encourage you to follow the <BasicPage.InnieLink to='/steps'><span>Steps to Participate</span></BasicPage.InnieLink> in <BasicPage.InnieLink to='/photo'>
+      <span>How to Send a Tick Pic</span>
+      </BasicPage.InnieLink> to begin the <BasicPage.InnieLink to='/preSurvey'>
+         <span>Tick Submission</span>
+       </BasicPage.InnieLink> process!
       </p>
 
-      <BasicPage.SectionTitle>View a report of the 11 ticks found throughout New Jersey.</BasicPage.SectionTitle>
+      <BasicPage.SectionTitle>Before sending us your ticks, view a report of the 11 ticks found throughout New Jersey.</BasicPage.SectionTitle>
       
         <p><TickReportLink /></p>
         <p>
-        Since this report, two more ticks have been discovered in NJ, which we review within <BasicPage.InnieLink  to='/ticks'><span>Ticks of NJ</span></BasicPage.InnieLink>
+        Since this report, two more ticks have been discovered in NJ, which we review in <BasicPage.InnieLink  to='/ticks'><span>Ticks of NJ</span></BasicPage.InnieLink>
       </p>
+      <BasicPage.ImageCont>
+        <figure>
+          <img src={tickCircle} alt='ticks' />
+          <figcaption>Image provided by Andrea Egizi, PhD
+              </figcaption>
+        </figure>
+      </BasicPage.ImageCont>
       <div>
         <BasicPage.CenteredText>
         <BasicPage.OutieLink href='https://vectorbio.rutgers.edu/contact.php' target='_blank' rel='noreferrer'><span>Visit the Center for Vector Biology Website</span></BasicPage.OutieLink>
