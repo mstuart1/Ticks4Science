c4a55f9dcf206cc42b304d91e944c3010a082d57
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-14 08:58:58 -0400

    Fix Rutgers Header styling

--- frontend/src/components/rutgersHeader/RutgersHeader.jsx
+++ frontend/src/components/rutgersHeader/RutgersHeader.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import headerLogo from "./RutgersHeaderLogo.svg";
-import { StyledHeader, StyledLogo, StyledMenu, WidthContainer } from "./RutgersHeader.styled";
+import styled from 'styled-components'
 
-const RutgersHeader = () => {
-  return (
-    <StyledHeader>
-      <WidthContainer>
-      <StyledLogo>
+const RutgersHeader = () => (
+    <Styles.Header>
+      <Styles.WidthContainer>
+      <Styles.Logo>
         <a href="https://www.rutgers.edu">
           <img src={headerLogo} alt="Rutgers Logo"  />
         </a>
-      </StyledLogo>
-      <StyledMenu>
+      </Styles.Logo>
+      <Styles.Menu>
         <ul>
           <li>
             <a href="https://newbrunswick.rutgers.edu/">
@@ -25,10 +24,58 @@ const RutgersHeader = () => {
             <a href="https://search.rutgers.edu/web.html">Search Rutgers</a>
           </li>
         </ul>
-      </StyledMenu>
-      </WidthContainer>
-    </StyledHeader>
+      </Styles.Menu>
+      </Styles.WidthContainer>
+    </Styles.Header>
   );
-};
+
 
 export default RutgersHeader;
+
+const Styles = {
+  Header: styled.div`
+  display: flex;
+  justify-content: center;
+  
+  font-family: 'Poppins', sans-serif;
+  
+  width: 100%;
+  padding: 0.25em 0.75em;
+  background: #fff;
+  a:link,
+  a:visited,
+  a:hover {
+      color: black;
+      text-decoration: none;
+      font-size: 1em;
+  }
+  a:hover {
+      text-decoration: underline;
+  }
+  `,
+  WidthContainer: styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  @media screen and (min-width:${({theme}) => theme.mobile}) {
+      max-width: 1366px;
+  }
+  `,
+  Logo: styled.div`
+img {
+    width: 17em;  
+    max-width: 464px; 
+}
+`,
+Menu: styled.div`
+li {
+    display: inline;
+    margin-right: 2em;
+    font-size: 0.75em;
+}
+@media screen and (max-width:${({theme}) => theme.mobile}) {
+ display: none;
+}
+`
+}
