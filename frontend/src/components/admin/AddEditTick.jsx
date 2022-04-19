079fb85b90af3739c5f8d37bc41e7e7c848d0c7d
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-19 14:58:30 -0400

    create array elements for add edit tick

--- frontend/src/components/admin/AddEditTick.jsx
+++ frontend/src/components/admin/AddEditTick.jsx
@@ -1,23 +1,9 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import { theme } from '../../theme'
 import { BasicPage } from '../GeneralStyles'
 import BorderlessInput from '../ui/borderlessInput/BorderlessInput'
-
-const Styles = {
-    Input: styled.input`
-    font-size: 1.4rem;
-    border: none;
-    -webkit-appearance: none;
-  -ms-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  background-color: ${({ colors }) => colors.bg};
-  color: ${({ colors }) => colors.text};
-  padding: 2rem 4rem;
-  border-radius: 3px;
-  margin: 2rem 0;
-    `
-}
+import TickDataService from '../../services/ticks'
+import { Navigate } from 'react-router-dom'
 
 const AddEditTick = () => {
 
@@ -33,27 +19,52 @@ const AddEditTick = () => {
         }));
     };
 
-    const handleSave = evt => {
-        evt.preventDefault()
-
+    const handleSave = async evt => {
+        try {
+            evt.preventDefault()
+            let response = await TickDataService.submitForm(input);
+            let id = response.data.data.id
+            setInput({})
+            Navigate(`/ticks/${id}`)
+        } catch(err) {
+            console.log(err.message)
+        }
     }
+    
+    let inputArray = [
+        {id: 1, placeholder: 'Scientific Name', name: 'scientific', value: input.scientific},
+        {id: 2, placeholder: 'Common Name', name: 'common', value: input.common},
+        {id: 3, name: 'colors', value: input.colors,placeholder: 'Appearance - Colors'},
+            {id: 4, name: 'shape', value: input.shape,placeholder: 'Appearance - Shape'},
+            {id: 5, name: 'hosts', value: input.hosts,placeholder: 'Hosts'},
+            {id: 6, name: 'habitat', value: input.habitat,placeholder: 'Habitat'},
+            {id: 7, name: 'njLocations', value: input.njLocations,placeholder: 'NJ Locations'},
+            {id: 8, name: 'larvaeActive', value: input.larvaeActive,placeholder: 'Larvae - When active?'},
+            {id: 9, name: 'larvaeFeed', value: input.larvaeFeed,placeholder: 'Larvae - Feed on?'},
+           
+            {id: 10, name: 'nymphActive', value: input.nymphActive,placeholder: 'Nymph - When active?'},
+            {id: 11, name: 'nymphFeed', value: input.nymphFeed,placeholder: 'Nymph - Feed on?'},
+           
+            {id: 12, name: 'adultActive', value: input.adultActive,placeholder: 'Adult - When active?'},
+            {id: 13, name: 'adultFeed', value: input.adultFeed,placeholder: 'Adult - Feed on?'},
+            {id: 14, name: 'intro', value: input.intro, placeholder: 'Intro text'},
+    ]
+
+    let inputElements = inputArray.map(item => (
+        <BorderlessInput key={item.id} placeholder={item.placeholder} errorText=''>
+            <input type="text" name={item.name} value={item.value} placeholder='&nbsp;' onChange={handleChange}/>
+        </BorderlessInput>
+    ))
 
     return (
         <BasicPage.Text>
-            <BorderlessInput onChange={handleChange} type='text' name='scientific' value={input.scientific || ''} placeholder='Scientific Name' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='common' value={input.common || ''} placeholder='Common Name(s)' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='colors' value={input.colors || ''} placeholder='Appearance - Colors' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='shape' value={input.shape || ''} placeholder='Appearance - Shape' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='hosts' value={input.hosts || ''} placeholder='Hosts' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='habitat' value={input.habitat || ''} placeholder='Habitat' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='njLocations' value={input.njLocations || ''} placeholder='NJ Locations' errorText='' />
-            <BorderlessInput onChange={handleChange} type='text' name='larvaeActive' value={input.larvaeActive || ''} placeholder='Larvae - When active?' errorText='' />
-            <Styles.Input onChange={handleChange} type='text' name='larvaeFeed' value={input.larvaeFeed || ''} placeholder='larvaeFeed' />
-            <Styles.Input onChange={handleChange} type='text' name='nymphActive' value={input.nymphActive || ''} placeholder='nymphActive' />
-            <Styles.Input onChange={handleChange} type='text' name='nymphFeed' value={input.nymphFeed || ''} placeholder='nymphFeed' />
-            <Styles.Input onChange={handleChange} type='text' name='adultActive' value={input.adultActive || ''} placeholder='adultActive' />
-            <Styles.Input onChange={handleChange} type='text' name='adultFeed' value={input.adultFeed || ''} placeholder='adultFeed' />
-            <button onClick={handleSave}>Save</button> 
+            {inputElements}
+            <BasicPage.HoverCard bgColor={theme.colors.main} onClick={handleSave}>
+            <BasicPage.LinkButton.CardSpecial >
+              <span >Submit</span>
+            </BasicPage.LinkButton.CardSpecial>
+          </BasicPage.HoverCard>
+
         </BasicPage.Text>
 
     )
