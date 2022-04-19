327f198f6a6d502bc053938abbeca6fa1ccdeb26
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-19 09:45:50 -0400

    convert buttons to internal/external link float buttons from ui

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { BasicPage } from "./GeneralStyles";
-import TickReportLink from "./ticks/TickReportLink";
+// import TickReportLink from "./ticks/TickReportLink";
 import tickCircle from '../images/Image017.jpg'
 import styled from "styled-components";
+import InternalLinkFloatButton from "./ui/internalLinkFloatButton/InternalLinkFloatButton";
+import ExternalLinkFloatButton from "./ui/externalLinkFloatButton/ExternalLinkFloatButton";
+import { theme } from '../theme'
 
 const About = () => {
   return (
@@ -17,57 +20,28 @@ const About = () => {
       </p>
       <Styles.CenterCntr>
         <div>
-        <div style={{margin: '1rem 0'}}>
-      <BasicPage.SectionTitle>Get Started</BasicPage.SectionTitle>
+          <div style={{ margin: '1rem 0' }}>
+            <BasicPage.SectionTitle>Get Started</BasicPage.SectionTitle>
 
-      <BasicPage.LinkButton.LinkSpec to={'/steps'}>
-        <BasicPage.HoverCard>
-          <BasicPage.LinkButton.CardSpecial>
-            <span style={{ fontWeight: "bold", textAlign: "center" }}> Follow the steps to participate</span>
+            <InternalLinkFloatButton colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/steps' text='Follow the steps to participate' />
 
-          </BasicPage.LinkButton.CardSpecial>
+            <InternalLinkFloatButton colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/photo' text='How to take a tick pic' />
 
-        </BasicPage.HoverCard>
-      </BasicPage.LinkButton.LinkSpec>
+            <InternalLinkFloatButton colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/preSurvey' text='Begin a submission' />
 
-
-      <BasicPage.LinkButton.LinkSpec to={'/photo'}>
-        <BasicPage.HoverCard>
-          <BasicPage.LinkButton.CardSpecial>
-            <span style={{ fontWeight: "bold", textAlign: "center" }}> How to take a tick pic</span>
-          </BasicPage.LinkButton.CardSpecial>
-
-        </BasicPage.HoverCard>
-      </BasicPage.LinkButton.LinkSpec>
-
-      <BasicPage.LinkButton.LinkSpec to={'/preSurvey'}>
-        <BasicPage.HoverCard>
-          <BasicPage.LinkButton.CardSpecial>
-            <span style={{ fontWeight: "bold", textAlign: "center" }}> Begin a submission</span>
-          </BasicPage.LinkButton.CardSpecial>
-
-        </BasicPage.HoverCard>
-      </BasicPage.LinkButton.LinkSpec>
-      {/* <p> 
+            {/* <p> 
        To begin a tick submission, we encourage you to follow the <BasicPage.InnieLink to='/steps'><span>Steps to Participate</span></BasicPage.InnieLink> in <BasicPage.InnieLink to='/photo'>
       <span>How to Send a Tick Pic</span>
       </BasicPage.InnieLink> to begin the <BasicPage.InnieLink to='/preSurvey'>
          <span>Tick Submission</span>
        </BasicPage.InnieLink> process!
       </p> */}
-      </div>
-      <div style={{margin: '5rem 0'}}>
-       <BasicPage.SectionTitle>If you have already submitted ticks </BasicPage.SectionTitle>
-      <BasicPage.LinkButton.LinkSpec to={'/progress'}>
-        <BasicPage.HoverCard>
-          <BasicPage.LinkButton.CardSpecial>
-            <span style={{ fontWeight: "bold", textAlign: "center" }}> Track your progress</span>
-          </BasicPage.LinkButton.CardSpecial>
-
-        </BasicPage.HoverCard>
-      </BasicPage.LinkButton.LinkSpec>
-      </div>
-      {/* <div style={{margin: '1rem 0'}}>
+          </div>
+          <div style={{ margin: '5rem 0' }}>
+            <BasicPage.SectionTitle>If you have already submitted ticks </BasicPage.SectionTitle>
+            <InternalLinkFloatButton colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Track your progress' />
+          </div>
+          {/* <div style={{margin: '1rem 0'}}>
       <BasicPage.SectionTitle>Before sending us your ticks</BasicPage.SectionTitle>
       <Styles.RptLnkBtn.LinkSpec href='https://vectorbio.rutgers.edu/outreach/Occi2019TickListNJ.pdf' target='_blank' rel='noreferrer'>
         <BasicPage.HoverCard>
@@ -83,25 +57,21 @@ const About = () => {
         Since this report, two more ticks have been discovered in NJ, which we review in <BasicPage.InnieLink to='/ticks'><span>Ticks of NJ</span></BasicPage.InnieLink>
       </p>
       </div> */}
-      </div>
-      <BasicPage.ImageCont>
-        <figure>
-          <img src={tickCircle} alt='ticks' style={{maxWidth: '350px'}} />
-          <figcaption>Image provided by Andrea Egizi, PhD
-          </figcaption>
-        </figure>
-      </BasicPage.ImageCont>
-      </Styles.CenterCntr> 
-      <div>
-        <BasicPage.CenteredText>
-          <BasicPage.OutieLink href='https://vectorbio.rutgers.edu/contact.php' target='_blank' rel='noreferrer'><span>Visit the Center for Vector Biology Website</span></BasicPage.OutieLink>
-        </BasicPage.CenteredText>
-      </div>
+        </div>
+        <BasicPage.ImageCont>
+          <figure>
+            <img src={tickCircle} alt='ticks' style={{ maxWidth: '350px' }} />
+            <figcaption>Image provided by Andrea Egizi, PhD
+            </figcaption>
+          </figure>
+        </BasicPage.ImageCont>
+      </Styles.CenterCntr>
 
-      <a href='mailto:cvb@rutgers.edu' target='_blank' rel='noreferrer'><BasicPage.StyledButton color='black'>Contact Us</BasicPage.StyledButton></a>
+
+      <ExternalLinkFloatButton colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='https://vectorbio.rutgers.edu/contact.php' text='Visit the Center for Vector Biology Website' />
+
+      <ExternalLinkFloatButton colors={{ text: 'black', shadow: '#000000', bg: theme.colors.ruYellow }} to='mailto:cvb@rutgers.edu' text='Contact Us' />
     </BasicPage.Text>
-
-
   );
 };
 
@@ -113,7 +83,7 @@ const Styles = {
   @media screen and (min-width: 1366px) {
    width: 1000px;
   }
-  @media screen and (max-width: ${({theme}) => theme.mobile}) {
+  @media screen and (max-width: ${({ theme }) => theme.mobile}) {
     flex-direction: column;
     width: 100vw;
     margin: 1rem;
