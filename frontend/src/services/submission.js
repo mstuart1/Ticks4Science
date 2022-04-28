08830d24f7a983d9df41b053a2f80eb251087e55
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-04-28 09:06:24 -0400

    add routes to submission service

--- frontend/src/services/submission.js
+++ frontend/src/services/submission.js
@@ -9,11 +9,19 @@ class SubmissionDataService {
         return http.post(`/api/submission`, data)
     }
     submitImage(id, formData, config) {
-        return http.put(`/api/submission/${id}`, formData, config)
+        return http.put(`/api/submission/photos/${id}`, formData, config)
     }
     getProgress(id) {
         // console.log('getting progress')
         return http.get(`/api/submission/${id}`)
+    }
+
+    getAllSubm() {
+        return http.get(`/api/submission`)
+    }
+    updateSub(data, id) {
+        console.log(data)
+        return http.put(`/api/submission/${id}`, data)
     }
 }
 
