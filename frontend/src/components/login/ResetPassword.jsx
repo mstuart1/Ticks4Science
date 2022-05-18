f8f1259af8a6fa2ee221ce259c3e0b2f00999aae
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-05-18 14:44:15 -0400

    create resetpassword component

--- frontend/src/components/login/ResetPassword.jsx
+++ frontend/src/components/login/ResetPassword.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-//TODO
+import React, { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {BasicPage} from '../GeneralStyles'
+import UserDataService from '../../services/users'
 
 const ResetPassword = () => {
+
+  let {token} = useParams()
+  
+  const [user, setUser] = useState(null)
+  
+  const getData = useCallback( async () => {
+    await UserDataService.getByToken(token)
+  }, [token]);
+  
+
+  useEffect(() => {
+    let data = getData()
+    setUser(data)
+  }, [getData])
+
+
+
   return (
-    <div>ResetPassword coming soon</div>
+    <BasicPage.Text>ResetPassword</BasicPage.Text>
   )
 }
 
