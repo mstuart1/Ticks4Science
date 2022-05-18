32cb08106dc34db06010a665c8e341ead25a5fc3
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-05-18 14:42:17 -0400

    add forgot controller

--- backend/api/user.controller.js
+++ backend/api/user.controller.js
@@ -102,7 +102,7 @@ exports.getByToken = async (req, res, next) => {
                 
                    
                  })
-                 return res.json({ data: user })
+                 return res.json({ user })
             
     } catch (err) {
         console.log(err.message)
@@ -155,6 +155,39 @@ exports.login = async (req, res, next) => {
         }
         res.header("Authorization", "Bearer " + accessToken);
         res.json({token: accessToken})
+    } catch(err) {
+        console.log(err.message)
+        next(err)
+    }
+}
+
+exports.forgot = async(req, res, next) => {
+    console.log(`@@@@----someone forgot their password ${req.body}---@@@@`)
+    try {
+        let {email} = req.body
+        let foundUser
+        await db.sequelize.transaction(async (t) => {
+            foundUser = await User.findOne({where: {
+                email
+            }},{transaction: t})
+        
+        if (foundUser === null) {
+           return res.json({message: 'BAD_USER'})
+          }
+          else {
+            accessToken = jwt.sign(
+                { id: foundUser.id, grant: foundUser.manageUsers},
+                process.env.SECRET_KEY,
+                { expiresIn: "24h" }
+              );
+              let message = `This email has been sent to you because the "forgot password" form was filled out.  If you did not fill out the "forgot password" form, please disregard this email.  If you did indeed forget your password, please click the link to reset it.  This link will expire 24 hours from when this email was sent.<a href='${process.env.CORS_ORIGIN}/admin/reset/${accessToken}'>Click this link</a>`
+              await mailHelper.sendMail(foundUser.email, message)
+              await User.update({resetToken: accessToken}, {where: {
+                  id: foundUser.id
+              }}, {transaction: t})
+          }
+        })
+        res.json({message: 'EMAIL_SENT'})
     } catch(err) {
         console.log(err.message)
         next(err)
