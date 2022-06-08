23e7e19ed47ad2cc126bdc7c2378e0c58170fd30
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-06-08 16:23:10 -0400

    #33 wrap app in provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from "./store"
 import App from './App';
 
 const rootElement = document.getElementById('root')
@@ -8,10 +10,12 @@ const basename = process.env.REACT_APP_BASENAME;
 
 ReactDOM.render(
     <React.StrictMode>
+        <Provider>
         <BrowserRouter 
         basename={basename}
         >
         <App/>
         </BrowserRouter>
+        </Provider>
     </React.StrictMode>, rootElement
 )
