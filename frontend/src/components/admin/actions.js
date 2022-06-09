54e58750cbad5a13cf76fda06b1cb17598ec214e
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-06-09 10:02:40 -0400

    add redux for token

--- frontend/src/components/admin/actions.js
+++ frontend/src/components/admin/actions.js
@@ -1,12 +1,39 @@
+import UserDataService from '../../services/users'
+
+
 export const CREATE_TOKEN = 'CREATE_TOKEN';
 
 export const createToken = token => ({
     type: CREATE_TOKEN,
-    payload: {token}
+    payload: { token }
 });
 
 export const REMOVE_TOKEN = 'REMOVE_TOKEN';
-export const removeToken = token => ({
-    type: REMOVE_TOKEN,
-    payload: {token}
+export const removeToken = () => ({
+    type: REMOVE_TOKEN
 })
+
+export const SET_ACTIVE_USER = 'SET_ACTIVE_USER';
+export const setActiveUser = user => ({
+    type: SET_ACTIVE_USER,
+    payload: { user }
+})
+
+export const loadToken = credentials => async dispatch => {
+    try {
+
+        let response = await UserDataService.loginUser(credentials);
+
+        let token = response.data.token
+        let user = response.data.data
+        dispatch(setActiveUser(user))
+        dispatch(createToken(token));
+
+    } catch (err) {
+        dispatch(displayAlert(JSON.stringify(err.response)));
+    }
+}
+
+export const displayAlert = text => () => {
+    alert(text);
+}
--- frontend/src/components/admin/reducers.js
+++ frontend/src/components/admin/reducers.js
@@ -1,6 +1,4 @@
-import { CREATE_TOKEN, REMOVE_TOKEN } from "./actions"
-
-
+import { CREATE_TOKEN, REMOVE_TOKEN, SET_ACTIVE_USER } from "./actions"
 
 // provide state to default to avoid errors
 export const token = (state = {}, action) => {
@@ -12,15 +10,24 @@ export const token = (state = {}, action) => {
         case CREATE_TOKEN: {
             // get the text defined in the payload (see actions if you are confused)
             const { token } = payload;
-           
-            // concat so you don't mutate the state - concat returns a new array
             return token;
         }
         case REMOVE_TOKEN: {
-            
             return {};
         }
         default:
             return state;
+    }
+}
+
+export const user = (state = {}, action) => {
+    const {type, payload} = action
+    switch (type) {
+        case SET_ACTIVE_USER: {
+            const {user} = payload;
+            return user;
+        }
+        default: 
+        return state;
     }
 }
