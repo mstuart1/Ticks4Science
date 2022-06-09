d6497db0464dab8d6e0de3b718b9241845331ef9
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-06-09 16:14:39 -0400

    add user to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {token} from './components/admin/reducers'
+import {token, user} from './components/admin/reducers'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
@@ -8,6 +8,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducers = {
     token,
+    user,
 };
 
 const persistConfig = {
