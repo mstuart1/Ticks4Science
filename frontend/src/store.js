3aff1dbfb90235f7d915b0edcdd3159f973bf2f5
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-06-09 08:41:52 -0400

    #33 add reducer to store, add persist to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,20 @@
 import {createStore, combineReducers} from 'redux';
+import {token} from './components/admin/reducers'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 
-const reducers = {};
+const reducers = {
+    token,
+};
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
 
 const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () => createStore(persistedReducer);
