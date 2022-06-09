6e6ba6f7ac14a238a4213acdf3f02cff91cd0da1
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-06-09 08:44:17 -0400

    #33 wrap app in persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from "./store"
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 import App from './App';
 
 const rootElement = document.getElementById('root')
 const basename = process.env.REACT_APP_BASENAME;
 
+const store = configureStore()
+const persistor = persistStore(store)
+
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={configureStore}>
+        <Provider store={store}>
+            <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
         <BrowserRouter 
         basename={basename}
         >
         <App/>
         </BrowserRouter>
+        </PersistGate>
         </Provider>
     </React.StrictMode>, rootElement
 )
