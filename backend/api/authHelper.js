ce0a9994afb85ed510b4bbe3d57af61513e19bce
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-06-14 10:51:43 -0400

    creaet auth helper #38

---
+++ backend/api/authHelper.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      res.status(401).send("No Auth Header.");
+    } else {
+      console.log(authHeader);
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) {
+          return res
+            .status(403)
+            .send({ code: "AUTH_ERROR", message: "Error authenticating token" });
+        } else {
+          user.expiresIn = "2h";
+          req.user = user;
+          let accessToken = jwt.sign(
+            { id: user.id, admin: user.admin },
+            process.env.SECRET_KEY,
+            { expiresIn: user.expiresIn }
+          );
+          res.header("X-REFRESH-TOKEN", accessToken); //previous: res.header("Authorization", "Bearer " + accessToken);
+          next();
+        }
+      });
+    }
+  };
+  
