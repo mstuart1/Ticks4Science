664523a39250ba7de9968965fccdd42774e28f13
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-07-08 09:27:41 -0400

    #58 change thank you wording

--- frontend/src/components/survey/ThankYou.jsx
+++ frontend/src/components/survey/ThankYou.jsx
@@ -16,7 +16,7 @@ const ThankYou = () => {
                 ><span>https://ticks.rutgers.edu/progress/{id}</span></BasicPage.InnieLink> */}
                 <h2>You must write down this number to access your tick submission!</h2>
                 <h1>{id}</h1>
-                <h2> Write this number down now!</h2>
+                <h2>Please this number down.</h2>
                 </BasicPage.SectionTitle>
             <p>
                 One of our tick experts will be examining the submitted photos and information about your tick! Be sure to save your personalized Tick ID number from your Tick Submission Form! You will use this number to access the website to check your progress.
