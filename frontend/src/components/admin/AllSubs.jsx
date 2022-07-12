af3d3f217db49cf1f3597ff038fb671c34713eaa
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-07-12 08:49:09 -0400

    add page, limit, and filter to data request

--- frontend/src/components/admin/AllSubs.jsx
+++ frontend/src/components/admin/AllSubs.jsx
@@ -1,48 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux'
-import { Link, useLocation
-  // , useNavigate
- } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { BasicPage } from "../GeneralStyles";
 import SubmissionDataService from '../../services/submission'
 import SubCard from './SubCard'
 import styled from "styled-components";
 import InternalLinkFloatButton from "../ui/internalLinkFloatButton/InternalLinkFloatButton";
-// import { theme } from "../../theme";
-// import { useDispatch } from "react-redux";
-
-// import { getOrgDataRequest } from "./actions";
-// import { PageContainer} from '../styles/Common.styled'
-// import { useDispatch, useSelector } from "react-redux";
-// import { CreateButton } from "../dataSources/DataSource.styled";
-// import { Instructions } from "./Orgs.styled";
-// import OrgTable from "./OrgTable"
 
 const Styles = {
-    Input: styled.input`
+  Input: styled.input`
     width: 80vw;
 
     @media screen and (min-width:${({ theme }) => theme.mobile}) {
         max-width: 800px;
       }
     `,
-    Link: styled(Link)`
+  Link: styled(Link)`
     text-decoration: none;
     color: black;
     `,
-    Card: styled.div`
+  Card: styled.div`
     margin: 1rem;
     padding: 1rem ; 
     width: 80vw;
     max-width: 900px;
-    border-radius: ${({theme}) => theme.borderRadius};
+    border-radius: ${({ theme }) => theme.borderRadius};
     box-shadow: 0 3px 15px ${({ shadowColor }) => shadowColor || '#000000'}20;
     `,
-    Waiting: styled.div`
+  Waiting: styled.div`
     display: flex;
     justify-content: space-between;
     `,
-    WaitingGroup: styled.div`
+  WaitingGroup: styled.div`
     margin: 2rem;
     padding: 2rem;
     background-color: #dfdfdf;
@@ -52,45 +41,44 @@ const Styles = {
 
 const AllSubs = () => {
 
-    const location = useLocation()
-    let filter = location.state?.filter || ""
+  const location = useLocation()
+  let filter = location.state?.filter || ""
 
-    const [data, setData] = useState([])
+  console.log(filter)
 
-    const token = useSelector(state => state.token.data)
-    
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(0)
+  const [limit, setLimit] = useState(3);
+  
 
-    
-    useEffect(() => {
-        let getData = async (token) => {
-        //token, page, limit
-         return await SubmissionDataService.getPageSubm(token,0,3);
-        }
-        
-        getData(token).then(response => {
-          // console.log(response.data.record)
-          setData(response.data.record)
-          
-        })
+  const token = useSelector(state => state.token.data)
+  
 
-        // if !data.tick && data.specimen
-        
-        
-      }, [token])
+  useEffect(() => {
+    let getData = async (token, page, limit, filter) => {
+      return await SubmissionDataService.getPageSubm(token, page, limit, filter);
+    }
 
-      
-      
-      let pendReceived =  data.filter(sub => !sub.duplicate && sub.specimenRequested && sub.specimenReceived === null);
-      let totalIdent = data.filter(sub => !sub.duplicate && sub.specimenIdentified)
+    getData(token, page, limit, filter).then(response => {
+      // console.log(response.data.record)
+      setData(response.data.record)
+
+    })
+  }, [token, page, limit, filter])
+
+
+
+  let pendReceived = data.filter(sub => !sub.duplicate && sub.specimenRequested && sub.specimenReceived === null);
+  let totalIdent = data.filter(sub => !sub.duplicate && sub.specimenIdentified)
   let pendingPhotos = data.filter(item => !item.duplicate && item.photosReviewed === null)
   let pendingSpecimens = data.filter(item => !item.duplicate && item.specimenReceived !== null && item.specimenIdentified === null)
 
   const createCardElems = data => {
 
     let cards = data.map(sub => (
-      <SubCard item={sub} key={sub.id}/>
-  ))
-  return cards
+      <SubCard item={sub} key={sub.id} />
+    ))
+    return cards
   }
 
   let totalCards = createCardElems(data)
@@ -100,55 +88,55 @@ const AllSubs = () => {
   let identCards = createCardElems(totalIdent)
 
   // console.log('totalIdent', totalIdent)
-  
-  return   (
+
+  return (
     <BasicPage.Text>
-       
-        <InternalLinkFloatButton padding="1rem 2rem" text='Back to Dashboard' to='/admin' />
-         <Styles.Waiting>
-          {filter === 'totalSubs' && (
-            <Styles.WaitingGroup>
-              <h2>All Submissions</h2>
-              {totalCards}
-            </Styles.WaitingGroup>
-          )}
-          {filter === 'pendPhotos' && (
-             <Styles.WaitingGroup>
-             <h2>Waiting for Photo Review</h2>
-             {photoCards}
-           </Styles.WaitingGroup>
-          )}
-          {filter === 'pendReceived' && (
-             <Styles.WaitingGroup>
-             <h2>Waiting for User to Mail In</h2>
-             {mailCards}
-           </Styles.WaitingGroup>
-          )}
-          {filter === 'pendIdentified' && (
-             <Styles.WaitingGroup>
-             <h2>Waiting for Lab to Identify Specimen</h2>
-             {specimenCards}
-           </Styles.WaitingGroup>
-          )}
-          {filter === 'totalIdent' && (
-             <Styles.WaitingGroup>
-             <h2>All Identified</h2>
-             {identCards}
-           </Styles.WaitingGroup>
-          )}
-         {filter === '' && (
-          <>
+
+      <InternalLinkFloatButton padding="1rem 2rem" text='Back to Dashboard' to='/admin' />
+      <Styles.Waiting>
+        {filter === 'totalSubs' && (
           <Styles.WaitingGroup>
-          <h2>Waiting for Photo Review</h2>
-          {photoCards}
-        </Styles.WaitingGroup>
-        <Styles.WaitingGroup>
-          <h2>Waiting for Specimen Review</h2>
-          {specimenCards}
-        </Styles.WaitingGroup>
-        </>
-         )}
-         </Styles.Waiting>
+            <h2>All Submissions</h2>
+            {totalCards}
+          </Styles.WaitingGroup>
+        )}
+        {filter === 'pendPhotos' && (
+          <Styles.WaitingGroup>
+            <h2>Waiting for Photo Review</h2>
+            {photoCards}
+          </Styles.WaitingGroup>
+        )}
+        {filter === 'pendReceived' && (
+          <Styles.WaitingGroup>
+            <h2>Waiting for User to Mail In</h2>
+            {mailCards}
+          </Styles.WaitingGroup>
+        )}
+        {filter === 'pendIdentified' && (
+          <Styles.WaitingGroup>
+            <h2>Waiting for Lab to Identify Specimen</h2>
+            {specimenCards}
+          </Styles.WaitingGroup>
+        )}
+        {filter === 'totalIdent' && (
+          <Styles.WaitingGroup>
+            <h2>All Identified</h2>
+            {identCards}
+          </Styles.WaitingGroup>
+        )}
+        {filter === '' && (
+          <>
+            <Styles.WaitingGroup>
+              <h2>Waiting for Photo Review</h2>
+              {photoCards}
+            </Styles.WaitingGroup>
+            <Styles.WaitingGroup>
+              <h2>Waiting for Specimen Review</h2>
+              {specimenCards}
+            </Styles.WaitingGroup>
+          </>
+        )}
+      </Styles.Waiting>
     </BasicPage.Text>
   )
 }
