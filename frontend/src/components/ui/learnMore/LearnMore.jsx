b1027002ce5a8165052086c4f74db4113da75741
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-07-13 09:40:54 -0400

    create LearnMore component

---
+++ frontend/src/components/ui/learnMore/LearnMore.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import InternalLinkFloatButton from '../internalLinkFloatButton/InternalLinkFloatButton'
+import QuestionsButton from '../questionsButton/QuestionsButton'
+
+const Styles = {
+    LearnCont: styled.div`
+    width: 100%;
+    margin: 1rem;
+    padding: 1rem;
+
+    font-size: 1.6rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    span {
+      text-align: center;
+    }
+    `,
+}
+
+const LearnMore = () => {
+  return (
+    <Styles.LearnCont>
+    <span>Want to learn more about New Jersey's tick species?</span>
+    <InternalLinkFloatButton to='/ticks' text='View Ticks of New Jersey'/>
+   <QuestionsButton/>
+   
+</Styles.LearnCont>
+  )
+}
+
+export default LearnMore
