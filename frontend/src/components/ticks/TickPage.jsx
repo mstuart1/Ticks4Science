088a372d3e72502b5f3e21a0792e97b9cbf8a5df
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-07-14 10:54:11 -0400

    add refImgElems

--- frontend/src/components/ticks/TickPage.jsx
+++ frontend/src/components/ticks/TickPage.jsx
@@ -111,21 +111,28 @@ const TickPage = () => {
      
   }, [])
 
-  console.log(data.refImgArray)
+  console.log(data.refImgArray?.images)
 
 
 
-  // let refImgElems = data.refImgArray?.length > 0 && data.refImgArray.map((img, i) =>{ 
-  //   if (!img.source.includes("local"))
-  //   return (
+  let refImgElems = data.refImgArray?.images.length > 0 && data.refImgArray.images.map((img, i) =>{ 
 
-  //   <figure key={i}>
-  //     <img src={img.source} alt='ticks' style={{ maxWidth: '350px', borderRadius: '5px', margin: '0.5rem' }} />
-  //     {/* <figcaption>{img.caption} */}
-  //     {/* </figcaption> */}
-  //   </figure>
-
-  // )})
+    console.log(img)
+    if (i > 0) {
+      
+      if (img.includes("http")){
+        return (
+          <img src={`${img}`} alt='ticks' style={{ maxWidth: '350px', maxHeight: '250px', borderRadius: '5px', margin: '0.5rem' }} key={i}/>
+        )
+      } else {
+        return (
+          <img src={process.env.PUBLIC_URL + img} alt='ticks' style={{ maxWidth: '350px', maxHeight: '250px', borderRadius: '5px', margin: '0.5rem' }} key={i}/>
+        )
+      }
+      
+    }
+    
+})
 
 
   return (
@@ -134,7 +141,7 @@ const TickPage = () => {
         <Styles.TopCont>
           <Styles.TopImgCont>
             {/* <div style={{backgroundColor: 'white', width: '200px', height: '400px'}}> </div> */}
-            <img src={data.featureImgUrl} alt='tick' style={{ maxWidth: '40vw' }} />
+            <img src={data.featureImgUrl?.includes("http") ? data.featureImgUrl : process.env.PUBLIC_URL + data.featureImgUrl} alt='tick' style={{ maxWidth: '40vw' }} />
           </Styles.TopImgCont>
           <Styles.TopInfoCont>
             <Styles.Section>
@@ -143,7 +150,7 @@ const TickPage = () => {
               <h2>Scientific Name:<br/> <i>{data.scientific}</i></h2>
             </Styles.Section>
             <Styles.Section>
-              <h3>{data.humanHosts && '⭐️ This tick can be found on people'}</h3>
+              <h3>{data.humanHosts && '⭐️ A frequent human biter'}</h3>
             </Styles.Section>
             <Styles.Section>
               {data.intro}
@@ -162,7 +169,7 @@ const TickPage = () => {
           </Styles.MiddleInfo>)}
         </Styles.MiddleCont>
         <Styles.MiddleCont>
-          {/* {refImgElems} */}
+          {refImgElems}
         </Styles.MiddleCont>
         <OutlineCard>
           <h3>A Reminder About Engorged Ticks</h3>
