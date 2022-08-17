0c0e1120bc7d7c4e4466f789629709fb21426e33
Michelle Stuart <michelle.stuart@rutgers.edu>
2022-08-17 15:56:22 -0400

    tweak travelInfo and additional info

--- frontend/src/components/admin/ProcessTick.jsx
+++ frontend/src/components/admin/ProcessTick.jsx
@@ -571,14 +571,14 @@ const ProcessTick = () => {
         </OutlineCard>
         </div>
         <SubTickInfo tick={tick}></SubTickInfo>
-       
+       {console.log('travel',tick.travelInfo)}
         <OutlineCard>
           <div style={{ margin: "1rem", padding: "1rem" }}>
             <h2>Submitter Info</h2>
             <p>Municipality: {tick.userMuni}</p>
             <p>Zip Code: {tick.userZip?.toString().padStart(5, "0")}</p>
-            <p>Travel History: {tick.travelInfo}</p>
-            <p>Additional Info: {tick.additionalInfo}</p>
+            <p>Travel History: {tick.travelInfo ? tick.travelInfo : 'not reported'}</p>
+            <p style={{maxWidth: '300px'}}>Additional Info: {tick.additionalInfo}</p>
           </div>
         </OutlineCard>
         <div>
