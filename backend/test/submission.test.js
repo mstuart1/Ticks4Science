fd269bc9e6d4470a673d5bfd9e03366b7b306abc
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-07 15:41:57 -0400

    prettier

--- backend/test/submission.test.js
+++ backend/test/submission.test.js
@@ -11,10 +11,10 @@ describe("Submission", () => {
   // update existing submission
   describe("PUT /submission/:id", () => {
     it("should add a userMuni to this submission", (done) => {
-     let id = 9
-     let newInfo = {
-      userMuni: 'Test Muni'
-     }
+      let id = 9
+      let newInfo = {
+        userMuni: 'Test Muni'
+      }
       chai
         .request(server)
         .put(`/submission/${id}`)
@@ -29,36 +29,36 @@ describe("Submission", () => {
     it("should add a userZip and pathogens to this submission", (done) => {
       let id = 9
       let newInfo = {
-       userZip: 26586,
-       pathogens: [1, 7, 8, 12]
+        userZip: 26586,
+        pathogens: [1, 7, 8, 12]
       }
-       chai
-         .request(server)
-         .put(`/submission/${id}`)
-         .send(newInfo)
-         .end((err, res) => {
-           // console.log(res.body);
-           res.should.have.status(200);
-           // res.body.should.have.property("code").eq("OK");
-           done();
-         });
-     })
+      chai
+        .request(server)
+        .put(`/submission/${id}`)
+        .send(newInfo)
+        .end((err, res) => {
+          // console.log(res.body);
+          res.should.have.status(200);
+          // res.body.should.have.property("code").eq("OK");
+          done();
+        });
+    })
     //  .timeout(5000000);
-    
+
   });
   // get downloadable data
-  describe.only("GET /submission/downloadData", () => {
+  describe("GET /submission/downloadData", () => {
     it("should return all of the information pertaining to all of the submissions", (done) => {
       let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZ3JhbnQiOnRydWUsImlhdCI6MTY1OTk2MzQ5MCwiZXhwIjoxNjU5OTcwNjkwfQ.OAoYdpioqLopRjFVUv1qSL5LwbHUma7LgLc5JaBi4cI'
       chai
-      .request(server)
-      .get('/submission/downloadData')
-      .send(token)
-      .end((err, res) => {
-        console.log(JSON.stringify(res.body, null, 1))
-        res.should.have.status(200)
-        done()
-      })
+        .request(server)
+        .get('/submission/downloadData')
+        .send(token)
+        .end((err, res) => {
+          console.log(JSON.stringify(res.body, null, 1))
+          res.should.have.status(200)
+          done()
+        })
     })
   })
 
