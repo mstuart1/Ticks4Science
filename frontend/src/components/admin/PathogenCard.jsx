5ff258ac3c39659eff21f869ad5eae5df7fe696b
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-07 15:43:13 -0400

    add save button and handle save

--- frontend/src/components/admin/PathogenCard.jsx
+++ frontend/src/components/admin/PathogenCard.jsx
@@ -2,6 +2,7 @@ import useAxios from "../../tools/useAxios";
 import PathogenDataService from '../../services/pathogens'
 import styled from "styled-components";
 import OutlineCard from "../ui/outlineCard/OutlineCard";
+import { theme } from "../../theme";
 
 
 const Styles = {
@@ -11,11 +12,36 @@ const Styles = {
     justify-content: flex-start;
     align-items: center;
   `,
+    SaveBtn: styled.button`
+    width: 100px; 
+    padding: 1rem;
+     color: white;
+     background: ${theme.colors.ruRed};
+     box-shadow: 0 1px 3px rgba(0, 0, 0, .1), 0 1px 2px rgba(0, 0, 0, .18);
+     border: none;
+    `,
+
 };
 
-const PathogenCard = ({ message = 'select all that tested positive', checkedPathogens = [], handleChange }) => {
+const PathogenCard = ({ message = 'select all that tested positive, you must hit the save button on this card to save the changes.', tick, handleChange }) => {
 
-    let { isError, isLoading, data: pathogens } = useAxios(PathogenDataService.getAll)
+    let { isError, isLoading, data } = useAxios(PathogenDataService.getAll)
+
+    if (isLoading) return <p>Loading...</p>
+    if (isError) return <p>Error!</p>
+
+    let { foundData: pathogens } = data
+
+    // todo add the list of current tick_Pathogen records to populate the checked boxes
+
+    const handleSave = (evt) => {
+        evt.preventDefault()
+        const formData = new FormData(evt.currentTarget)
+        // const formObject = Object.fromEntries(formData) // This gives us {Eewing: '6', Rpark: '9', BRBV: '14'}, we want the values {'6', "9", "14"}
+        const values = [...formData.values()]
+        console.log('Pathogen Card', values)
+        handleChange(values)
+    }
 
     let pathogenElems =
         pathogens?.length > 0 &&
@@ -27,21 +53,22 @@ const PathogenCard = ({ message = 'select all that tested positive', checkedPath
                     name={patho.pathogen}
                     value={patho.id}
                     style={{ margin: "1rem" }}
-                    checked={checkedPathogens.length && (checkedPathogens.some((value) => value.id === patho.id) || "")}
-                    onChange={handleChange}
+
+                // checked={checkedPathogens.length && (checkedPathogens.some((value) => value.id === patho.id) || "")}
+                // onChange={handleChange}
                 />
                 <label htmlFor={patho.pathogen}>{patho.pathogen}</label>
             </Styles.PathosCont>
         ));
 
-    if (isLoading) return <p>Loading...</p>
-    if (isError) return <p>Error!</p>
-
     return (
-        <OutlineCard width="300px">
+        <OutlineCard>
             <h2>Pathogens</h2>
             <p>{message}</p>
-            {pathogenElems}
+            <form onSubmit={handleSave}>
+                {pathogenElems}
+                <Styles.SaveBtn type='submit'>Save</Styles.SaveBtn>
+            </form>
         </OutlineCard>
     )
 }
