1ac99a797f07561a0cafa001a69a779be8506bf4
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-09 06:55:04 -0400

    add tests to submission

--- backend/test/submission.test.js
+++ backend/test/submission.test.js
@@ -61,6 +61,39 @@ describe("Submission", () => {
         })
     })
   })
-
-
+  // update submission pathogens
+  describe("PUT /submission/:id/pathos", () => {
+    it("should update the pathogens associated with this submission", (done) => {
+      let id = 19
+      let pathogens = [{ id: 1 }, { id: 7 }, { id: 8 }, { id: 12 }]
+      chai
+        .request(server)
+        .put(`/submission/${id}/pathos`)
+        .send({ pathogens })
+        .end((err, res) => {
+          console.log(res.body);
+          res.should.have.status(200);
+          // res.body.should.have.property("code").eq("OK");
+          done();
+        });
+    });
+  }
+  )
+  // update submission pathogens result
+  describe("PUT /submission/:id/result", () => {
+    it("should update the pathogen results associated with this submission", (done) => {
+      let id = 19
+      let data = { pathogenId: 1, result: 'positive' }
+      chai
+        .request(server)
+        .put(`/submission/${id}/result`)
+        .send(data)
+        .end((err, res) => {
+          console.log(res.body);
+          res.should.have.status(200);
+          // res.body.should.have.property("code").eq("OK");
+          done();
+        });
+    });
+  })
 });
