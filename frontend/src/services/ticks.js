e7dabe6cb4f03a24debdfd2c4573403ff65c2dfb
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-09 05:38:11 -0400

    #111 add service to update tick_pathogen relationship

--- frontend/src/services/ticks.js
+++ frontend/src/services/ticks.js
@@ -9,7 +9,7 @@ class TickDataService {
     submitForm(data) {
         return http.post(`/tick`, data)
     }
-    updateForm(data, id){
+    updateForm(data, id) {
         return http.put(`/tick/${id}`, data)
     }
     getTick(id) {
@@ -19,7 +19,13 @@ class TickDataService {
         // console.log(`getting all ticks`)
         return http.get(`/tick`)
     }
-    
+    updatePathos(tickId, pathogenId) {
+        /** this is going to send one id, if that id is already in the list, 
+         * it will remove it, if it is not in the list, it will add it
+         */
+        return http.put(`/tick/${tickId}/pathos`, pathogenId)
+    }
+
 }
 
 export default new TickDataService();
