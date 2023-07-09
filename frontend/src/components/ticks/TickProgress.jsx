6908f2792c3910e62fedb360cbebb52074ab554f
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-09 08:19:35 -0400

    #111 update TickProgress to show pathogen tests

--- frontend/src/components/ticks/TickProgress.jsx
+++ frontend/src/components/ticks/TickProgress.jsx
@@ -8,6 +8,63 @@ import InternalLinkFloatButton from '../ui/internalLinkFloatButton/InternalLinkF
 import ExternalLinkFloatButton from '../ui/externalLinkFloatButton/ExternalLinkFloatButton'
 import { theme } from '../../theme'
 
+const Styles = {
+  TimelineIcon: styled.div`
+    position: absolute;
+    margin-top: 4rem;
+    left: -27px;  // check and adjust as needed - can use browser console to tweak until you get the right value
+    top: 0;
+    background-color: ${({ theme }) => theme.colors.main};
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    i {
+        font-size: 2rem;
+        color: white;
+    }
+  `,
+  Timeline: styled.div`
+    display: flex;
+   flex-direction: column;
+    padding-bottom: 3rem;
+    margin-left: 3rem;
+    .timeline-item {
+        position: relative;
+        padding: 3rem;
+        border-left: 1px solid ${({ theme }) => theme.colorGrey5};
+        
+        .tl-duration {
+            padding: .2rem .6rem;
+            background-color: ${({ theme }) => theme.colorGrey5};
+            border-radius: 15px;
+            display: inline-block;
+            font-size: 1.6rem;
+            text-transform: uppercase;
+            font-weight: 500;
+        }
+        h3 {
+            padding: 1rem 0;
+            text-transform: uppercase;
+            font-size: 1.3rem;
+            font-weight: 600;
+            span {
+                color: ${({ theme }) => theme.colorGrey2};
+                font-weight: 500;
+                font-size: 1.2rem;
+            }
+        }
+        p {
+            color: ${({ theme }) => theme.colorGrey2};
+        }
+
+  
+}
+`
+}
+
 const TickProgress = () => {
   let { id } = useParams()
 
@@ -156,6 +213,15 @@ const TickProgress = () => {
                     {data.lifeStage && <p>
                       Your tick was at the life stage {data.lifeStage} and was {!data.engorged && 'not'} engorged.
                     </p>}
+                    {console.log('data.specimen', data)}
+                    {/* // Todo once the team updates the names of all of the pathogens, can remove the abbreviations  */}
+                    Your tick will be tested for the following pathogens.  The results will be posted here when they are available.
+                    <ul>
+                      {data.pathogens.map(pathogen => (
+                        <li key={pathogen.id}>{pathogen.name} - {pathogen.pathogen} - {pathogen.submission_pathogen.result}</li>
+                      ))
+                      }
+                    </ul>
                   </div>
                 )}
               </div>
@@ -175,59 +241,3 @@ const TickProgress = () => {
 
 export default TickProgress
 
-const Styles = {
-  TimelineIcon: styled.div`
-    position: absolute;
-    margin-top: 4rem;
-    left: -27px;  // check and adjust as needed - can use browser console to tweak until you get the right value
-    top: 0;
-    background-color: ${({ theme }) => theme.colors.main};
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    i {
-        font-size: 2rem;
-        color: white;
-    }
-  `,
-  Timeline: styled.div`
-    display: flex;
-   flex-direction: column;
-    padding-bottom: 3rem;
-    margin-left: 3rem;
-    .timeline-item {
-        position: relative;
-        padding: 3rem;
-        border-left: 1px solid ${({ theme }) => theme.colorGrey5};
-        
-        .tl-duration {
-            padding: .2rem .6rem;
-            background-color: ${({ theme }) => theme.colorGrey5};
-            border-radius: 15px;
-            display: inline-block;
-            font-size: 1.6rem;
-            text-transform: uppercase;
-            font-weight: 500;
-        }
-        h3 {
-            padding: 1rem 0;
-            text-transform: uppercase;
-            font-size: 1.3rem;
-            font-weight: 600;
-            span {
-                color: ${({ theme }) => theme.colorGrey2};
-                font-weight: 500;
-                font-size: 1.2rem;
-            }
-        }
-        p {
-            color: ${({ theme }) => theme.colorGrey2};
-        }
-
-  
-}
-`
-}
