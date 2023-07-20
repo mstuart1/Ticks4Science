8f9993ae8ae1449b6147c52bf60cc8248183ed16
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-20 08:29:50 -0400

    style the steps

--- frontend/src/components/Steps.jsx
+++ frontend/src/components/Steps.jsx
@@ -28,13 +28,16 @@ const StepStyles = {
   CenterCntr: styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
+    // flexWrap: wrap;
     // @media screen and (min-width: 1366px) {
     // //  max-width: 1000px;
     // }
     @media screen and (max-width: ${({ theme }) => theme.mobile}) {
       flex-direction: column;
       // width: 100vw;
-      margin: 1rem;
+      // margin: 1rem;
+      // flexWrap: wrap;
      }
     `,
 }
@@ -43,26 +46,26 @@ const Steps = () => (
 
   <BasicPage.Text>
     <BasicPage.Title>Join NJ Ticks 4 Science!</BasicPage.Title>
-    <StepStyles.CenterCntr >
-      <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap-reverse' }}>
-        <div>
-
-          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/tickOrInsect' text='Are you sure it is a tick?' />
-          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/photo' text='Learn to take tick pics' />
-        </div>
-        <div >
-          <p>Here's what to do:</p>
-          <BasicPage.CardContainer >
-
-
-            <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/preSurvey' text='Step 1 - Fill out the form and upload your pics' />
-            <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/missingId' text='Step 2 - Write down tick identification number' />
-            <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Step 3 - Check our website for a tick request (usually 24hrs or less)' />
-            <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Step 4 - Mail tick in' />
-            <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Step 5 - Check our website for results in a few weeks' />
-          </BasicPage.CardContainer>
-        </div>
+    {/* <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap-reverse', justifyContent: 'center' }}> */}
+    <StepStyles.CenterCntr>
+      <div>
+        <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/tickOrInsect' text='Are you sure it is a tick?' />
+        <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/photo' text='Learn to take tick pics' />
       </div>
+      <div >
+
+        <p>Here's what to do:</p>
+        <BasicPage.CardContainer >
+
+
+          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/preSurvey' text='Step 1 - Fill out the form and upload your pics' />
+          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/missingId' text='Step 2 - Write down tick identification number' />
+          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Step 3 - Check our website for a tick request (usually 24hrs or less)' />
+          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Step 4 - Mail tick in' />
+          <InternalLinkFloatButton width="30rem" colors={{ text: theme.colors.ruTeal, shadow: theme.colors.ruTeal }} to='/progress' text='Step 5 - Check our website for results in a few weeks' />
+        </BasicPage.CardContainer>
+      </div>
+
       <BasicPage.ImageCont>
         <figure>
           <img src={circle} alt='circle of ticks' />
@@ -71,6 +74,7 @@ const Steps = () => (
         </figure>
       </BasicPage.ImageCont>
     </StepStyles.CenterCntr>
+    {/* </div> */}
   </BasicPage.Text>
 
 )
