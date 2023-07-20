7920f47dacf921d1438a501d815f87534a9844f7
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-20 07:22:16 -0400

    change how status card assigns all ticks
    was getting error at this line that it is not iterable

--- frontend/src/components/admin/StatusCard.jsx
+++ frontend/src/components/admin/StatusCard.jsx
@@ -112,8 +112,9 @@ const StatusCard = ({ tick, updateSub, user, id }) => {
         let getData = async () => await TickDataService.getAllTicks();
 
         getData().then((response) => {
-            // console.log('processTicks.getData', response.data)
-            setTickSpp([...response.data.allTicks]);
+            console.log('processTicks.getData', response.data.allTicks)
+
+            setTickSpp(response.data.allTicks);
         });
     }, []);
 
