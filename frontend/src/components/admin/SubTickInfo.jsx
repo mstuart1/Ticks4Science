234ebd87c6df2921bebe469038065b3537ec1fef
Michelle Stuart <michelle.stuart@rutgers.edu>
2023-07-25 09:24:42 -0400

    import the outlineCard styles so a flex grow can be applied #125

--- frontend/src/components/admin/SubTickInfo.jsx
+++ frontend/src/components/admin/SubTickInfo.jsx
@@ -2,30 +2,52 @@ import React from 'react'
 import styled from 'styled-components'
 import OutlineCard from '../ui/outlineCard/OutlineCard'
 
+
+let SpecOutline = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+border-radius: ${({ theme }) => theme.borderRadius};
+border: 2px solid ${({ bgColor }) => bgColor + 30 || 'grey'};
+background-color: ${({ bgColor }) => bgColor + 20 || 'white'};
+padding: 2rem; 
+margin: 1rem;
+width: ${({ width }) => width};
+height: ${({ height }) => height};
+flex-grow: 1;
+`
 const Styles = {
-    Container: styled.div`
+  Container: styled.div`
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     p {
-        margin: 0.5rem;
-    }
+        margin: 1rem;
+    },
+
     `,
+  // SpecOutline: styled.div`
+  // ${StyledCard} {
+
+  // }`
+  
 }
 
-const SubTickInfo = ({tick}) => {
+
+const SubTickInfo = ({ tick }) => {
   return (
-    <OutlineCard>
-        <h2>Tick Info</h2>
-        <Styles.Container>
-        <p>Date Tick Found: {tick.dateTickFound?.substring(0,10)}</p>
+    <SpecOutline >
+      <h2>Tick Info</h2>
+      <Styles.Container>
+        <p>Date Tick Found: {tick.dateTickFound?.substring(0, 10)}</p>
         <p>Found On: {tick.foundOn}</p>
         <p>Attached: {tick.tickAttached}</p>
         <p>Found in Municipality: {tick.tickMuni}</p>
         <p>Found in Zip Code: {tick.tickZip?.toString().padStart(5, "0")}</p>
         <p>Activities: {tick.activities}</p>
-        </Styles.Container>
-    </OutlineCard>
+      </Styles.Container>
+    </SpecOutline>
   )
 }
 
