127717be34b8f94def29cf2eb830716295204733
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-16 11:07:08 -0500

    clear form on submit #136

--- frontend/src/components/QuestionSidebar/QuestionSidebar.jsx
+++ frontend/src/components/QuestionSidebar/QuestionSidebar.jsx
@@ -3,7 +3,7 @@ const QuestionSidebar = ({subId}) => {
     // todo create send question function
 
     const handleQuestion = (evt) => {
-        evt.preventDefault()
+        // evt.preventDefault()
         const formData = new FormData(evt.currentTarget)
         formData.append('subId', subId)
         formData.append('role', 'submitter')
@@ -18,7 +18,7 @@ const QuestionSidebar = ({subId}) => {
         <h2 className={styles.title}>Questions?</h2>
         <p className={styles.text}>Enter any questions or comments in the box below.  Please DO NOT enter any personal information.  The response to your question/comment will appear here.</p>
         <form  onSubmit={handleQuestion}>
-            <textarea className={styles.textarea} id="message" name="message" rows="4" cols="30" placeholder="Drag the bottom corner to create more space if needed."></textarea>
+            <textarea className={styles.textarea} defaultValue='' id="message" name="message" rows="4" cols="30" placeholder="Drag the bottom right corner to create more space if needed."></textarea>
             <br />
             <button className={styles.button} type="submit" value="Submit" ><span>Submit</span></button>
         </form>
