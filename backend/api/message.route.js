f38c2b0f63e06411ce73f6b234f1ae0c45937a6d
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-16 11:30:56 -0500

    add post route #136

--- backend/api/message.route.js
+++ backend/api/message.route.js
@@ -1,16 +1,16 @@
 module.exports = (app) => {
-    // const SubmCtl = require("./submission.controller");
+    const MessageCtl = require("./message.controller");
     const router = require("express").Router();
     // const upload = require('./upload');
 
     router
+    .post("/", MessageCtl.createMessage)
         // .get("/", auth.verifyToken, SubmCtl.getAllSubs)
         // .get("/paged", auth.verifyToken, SubmCtl.getSubPage)
         // .get("/downloadData", SubmCtl.downloadData)
         // .get('/deleted', auth.verifyToken, SubmCtl.getDeleted)
         // .get('/dupes', auth.verifyToken, SubmCtl.getDupes)
         // .get('/:id', SubmCtl.getProgress)
-        // .post("/", SubmCtl.createSubm)
         // .post("/photos/:id", upload.array('photos', 2), SubmCtl.uploadPhoto)
         // .put('/:id/pathos', SubmCtl.updatePathos)
         // .put('/:id/result', SubmCtl.updateResult)
