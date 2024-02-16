2c17fc8d603d45f4e58077a274857835071ca1ab
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-16 12:49:12 -0500

    handle answered #136

--- frontend/src/components/admin/MessagesCard/MessagesCard.jsx
+++ frontend/src/components/admin/MessagesCard/MessagesCard.jsx
@@ -1,11 +1,12 @@
 import OutlineCard from "../../ui/outlineCard/OutlineCard"
 import styles from './messages.module.css'
+import MessageDataService from '../../../services/message'
 
 const MessagesCard = ({messages, user, submissionId}) => {
   
-  const handleAnswered = async (id) => {
-    console.log('id', id)
-    // todo create update message function
+  const handleAnswered = async (id, evt) => {
+    // console.log('evt', evt.target.checked)
+    let response = await MessageDataService.updateMessage({id, answered: evt.target.checked})
   }
 
   return (
@@ -18,7 +19,7 @@ const MessagesCard = ({messages, user, submissionId}) => {
             <div className={styles.messageDiv}><div key={message.id} className={styles.text}><span className={styles.date}>{new Date(message.createdAt).toString()} from {message.role}:</span><br /> <span className={styles.highlight}>{message.message}</span></div>
            {message.role === 'submitter' && <>
               <label htmlFor='answered'>Answered? {'   '}</label>
-              <input  type='checkbox' id='answered' name='answered' value={message.answered} onChange={() => handleAnswered(message.id)} />
+              <input  type='checkbox' id='answered' name='answered' value={message.answered} onChange={(evt) => handleAnswered(message.id, evt)} />
               </>
           }
             </div>)
