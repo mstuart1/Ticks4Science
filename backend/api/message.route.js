136f3b572c038a4df644757aa5ed21ff8968299f
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-16 11:21:49 -0500

    create message route holder #136

---
+++ backend/api/message.route.js
@@ -0,0 +1,21 @@
+module.exports = (app) => {
+    // const SubmCtl = require("./submission.controller");
+    const router = require("express").Router();
+    // const upload = require('./upload');
+
+    router
+        // .get("/", auth.verifyToken, SubmCtl.getAllSubs)
+        // .get("/paged", auth.verifyToken, SubmCtl.getSubPage)
+        // .get("/downloadData", SubmCtl.downloadData)
+        // .get('/deleted', auth.verifyToken, SubmCtl.getDeleted)
+        // .get('/dupes', auth.verifyToken, SubmCtl.getDupes)
+        // .get('/:id', SubmCtl.getProgress)
+        // .post("/", SubmCtl.createSubm)
+        // .post("/photos/:id", upload.array('photos', 2), SubmCtl.uploadPhoto)
+        // .put('/:id/pathos', SubmCtl.updatePathos)
+        // .put('/:id/result', SubmCtl.updateResult)
+        // .put('/:id', SubmCtl.updateSubm)
+        // .delete('/:id', SubmCtl.deleteSub)
+
+    app.use("/message", router);
+};
