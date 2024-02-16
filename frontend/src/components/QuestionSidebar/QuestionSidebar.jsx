aaee715457acfd24c163914cd85ca5e45dd3f024
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-16 12:13:02 -0500

    add message data service to create question #136

--- frontend/src/components/QuestionSidebar/QuestionSidebar.jsx
+++ frontend/src/components/QuestionSidebar/QuestionSidebar.jsx
@@ -1,14 +1,18 @@
 import styles from './question.module.css'
-const QuestionSidebar = ({subId}) => {
+import MessageDataService from '../../services/message'
+const QuestionSidebar = ({subId, subMessages}) => {
+
+  console.log('subId', subId, 'subMessages', subMessages)
     // todo create send question function
 
-    const handleQuestion = (evt) => {
+    const handleQuestion = async (evt) => {
         // evt.preventDefault()
         const formData = new FormData(evt.currentTarget)
-        formData.append('subId', subId)
+        formData.append('submissionId', subId)
         formData.append('role', 'submitter')
         const formObject = Object.fromEntries(formData)
-        console.log('Question', formObject)
+        // console.log('Question', formObject)
+      let response = await MessageDataService.createMessage(formObject);
         
     }
     // todo create fetch message history function
@@ -23,10 +27,12 @@ const QuestionSidebar = ({subId}) => {
             <button className={styles.button} type="submit" value="Submit" ><span>Submit</span></button>
         </form>
           
-          {!!messages &&  <><h2 className={styles.title}>Message History</h2>
-        <ul>
-            <li className={styles.text}>Message history</li>
-              </ul></>}
+          {!!subMessages.length &&  <>
+          <h2 className={styles.title}>Message History</h2>
+            <ul className={styles.list}>
+          {subMessages.map((message) => (<li key={message.id} className={styles.text}><span className={styles.date}>{new Date(message.createdAt).toString()} from {message.role}:</span><br/> <span className={styles.highlight}>{message.message}</span></li>)
+              )}
+            </ul></>}
     </div>
   )
 }
