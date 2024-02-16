b321b48d88e92236b79e2c397014f66a4bfdf7c2
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-16 13:09:29 -0500

    handle admin responses to user #136

--- frontend/src/components/admin/MessagesCard/MessagesCard.jsx
+++ frontend/src/components/admin/MessagesCard/MessagesCard.jsx
@@ -3,30 +3,50 @@ import styles from './messages.module.css'
 import MessageDataService from '../../../services/message'
 
 const MessagesCard = ({messages, user, submissionId}) => {
+// console.log('messages', messages, 'user', user, 'submissionId', submissionId)
+  let sortedMessages = messages?.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
   
   const handleAnswered = async (id, evt) => {
     // console.log('evt', evt.target.checked)
     let response = await MessageDataService.updateMessage({id, answered: evt.target.checked})
   }
 
+  const handleQuestion = async (evt) => {
+    // evt.preventDefault()
+    const formData = new FormData(evt.currentTarget)
+    formData.append('submissionId', submissionId)
+    formData.append('role', 'admin')
+    formData.append('adminId', user.id)
+    formData.append('answered', true)
+    const formObject = Object.fromEntries(formData)
+    // console.log('Question', formObject)
+    let response = await MessageDataService.createMessage(formObject);
+
+  }
+
   return (
     <OutlineCard width="30rem">
         {/* <h2>Messages</h2> */}
-      {!!messages.length && <>
+      {!!sortedMessages && !!sortedMessages.length && <>
         <h2 className={styles.title}>Message History</h2>
         
-          {messages.map((message) => (
-            <div className={styles.messageDiv}><div key={message.id} className={styles.text}><span className={styles.date}>{new Date(message.createdAt).toString()} from {message.role}:</span><br /> <span className={styles.highlight}>{message.message}</span></div>
+          {sortedMessages.map((message) => (
+            <div key={message.id} className={styles.messageDiv}><div className={styles.text}><span className={styles.date}>{new Date(message.createdAt).toString()} from {message.role === 'submitter' ? message.role : message.admin.firstName}:</span><br /> <span className={styles.highlight} style={{ background: message.role === 'admin' ? '#fff6d4' : '#e3f3ef'}}>{message.message}</span></div>
            {message.role === 'submitter' && <>
               <label htmlFor='answered'>Answered? {'   '}</label>
-              <input  type='checkbox' id='answered' name='answered' value={message.answered} onChange={(evt) => handleAnswered(message.id, evt)} />
+              <input  type='checkbox' id='answered' name='answered' checked={message.answered} onChange={(evt) => handleAnswered(message.id, evt)} />
               </>
           }
             </div>)
           )}
         
         </>}
-
+      <h2 className={styles.title}>Add a message to the submission</h2>
+      <form onSubmit={handleQuestion}>
+        <textarea className={styles.textarea} defaultValue='' id="message" name="message" rows="4" cols="30" placeholder="Drag the bottom right corner to create more space if needed."></textarea>
+        <br />
+        <button className={styles.button} type="submit" value="Submit" ><span>Submit</span></button>
+      </form>
     </OutlineCard>
   )
 }
