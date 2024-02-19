8106c458cd3d5bb0255e571f48393de007f28e24
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-19 09:46:42 -0500

    update the footer logo

--- frontend/src/components/rutgersFooter/RutgersFooter.jsx
+++ frontend/src/components/rutgersFooter/RutgersFooter.jsx
@@ -1,141 +1,65 @@
-import styled from "styled-components";
 import React from "react";
-import footerLogo from "./RutgersFooterLogo.svg";
-
+import footerLogo from "./RU_H_2LINE_WHITE.png";
+import styles from './ruFooter.module.css'
 
 const RutgersFooter = () => {
-
   return (
-    
-      <Styles.FooterContainer>
-        <Styles.WidthContainer>
-          <Styles.Header>
-          <Styles.Logo>
-            <a href="https://www.rutgers.edu">
-              <img src={footerLogo} alt="Rutgers Logo" />
-            </a>
-          </Styles.Logo>
-          <Styles.Menu>
-            <ul>
-              <li>
-                <a href="https://newbrunswick.rutgers.edu/">New Brunswick</a>
-              </li>
-              <li>
-                <a href="https://academichealth.rutgers.edu/">RBHS/Health</a>
-              </li>
-              <li>
-                <a href="https://newark.rutgers.edu/">Newark</a>
-              </li>
-              <li>
-                <a href="https://www.camden.rutgers.edu/">Camden</a>
-              </li>
-            </ul>
-          </Styles.Menu>
-        </Styles.Header>
-        
-        <Styles.Disclaimer>
-        <p>Rutgers is an equal access/equal opportunity institution. Individuals with disabilities are encouraged to direct suggestions, comments, or complaints concerning any accessibility issues with Rutgers websites to&nbsp;<a href="mailto:accessibility@rutgers.edu">accessibility@rutgers.edu</a>&nbsp;or complete the&nbsp;<a href="https://it.rutgers.edu/it-accessibility-initiative/barrierform/" target="_blank" rel="noreferrer">Report Accessibility Barrier / Provide Feedback</a>&nbsp;form.</p>
+    <footer className={styles.footer}>
+      <div className={styles.widthCont}>
+        <section className={styles.header}>
+          <FooterLogo />
+          <FooterNav />
+        </section>
+        <FooterDisclaimer />
+      </div>
+    </footer>
 
-        <p><a href="/copyright-information" target="_blank" rel="noreferrer">Copyright ©{new Date().getFullYear()}</a>, Rutgers, The State University of New Jersey. All rights reserved.&nbsp;<br/><a href="mailto:ora-it@njaes.rutgers.edu">Contact webmaster</a></p>
-        </Styles.Disclaimer>
-        </Styles.WidthContainer>
-      </Styles.FooterContainer>
-    
   );
 };
 
 export default RutgersFooter;
 
+const FooterNav = () => (
 
-const Styles = {
-  FooterContainer: styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  background: #000;
-  height: 15rem; // room for mobile nav bar
-  a:link,
-  a:visited,
-  a:hover {
-    color: white;
-    text-decoration: none;
-    font-size: 1em;
-  }
-  a:hover {
-    text-decoration: underline;
-  }
-`,
-WidthContainer: styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100vw;
-  @media screen and (min-width:${({theme}) => theme.mobile}) {
-      max-width: 1366px;
-  }
-  `,
-Header: styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  padding: 3em 0.75em 1em 0.75em;
-  
-  @media screen and (max-width:${({theme}) => theme.mobile}) {
-    display: none;
-}
-`,
-Logo: styled.div`
-  img {
-    width: 13em;
-  }
-  @media screen and (max-width:${({theme}) => theme.mobile}) {
-    display: none;
-}
-`,
-Menu: styled.div`
-  li {
-    display: inline;
-    margin-right: 2em;
-    font-size: 0.75em;
-    padding-right: 2em;
-    border-right: 0.25px solid grey;
-  }
-  @media screen and (max-width:${({theme}) => theme.mobile}) {
-    display: none;
-}
-`,
-FooterMenuContainer: styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  border-bottom: 0.25px solid grey;
-  justify-content: space-evenly;
-`,
-SingleMenu: styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1em;
-  color: white;
-  font-size: 0.75em;
-  font-weight: 200;
-  line-height: 4em;
-//   height: 400px;
-//   flex-wrap: wrap;
-`, 
-Disclaimer: styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  font-size: 0.75em;
-p {
-    text-align: center;
-}
-`
-}
+  <nav className={styles.footerNav}>
+    <ul>
+      <li>
+        <a href="https://newbrunswick.rutgers.edu/">New Brunswick</a>
+      </li>
+      <li>
+        <a href="https://academichealth.rutgers.edu/">RBHS/Health</a>
+      </li>
+      <li>
+        <a href="https://newark.rutgers.edu/">Newark</a>
+      </li>
+      <li>
+        <a href="https://www.camden.rutgers.edu/">Camden</a>
+      </li>
+    </ul>
+  </nav>
+)
 
 
+const FooterLogo = () => (
+
+  <div className={styles.logoCont}>
+    <a href="https://www.rutgers.edu">
+      {/* 150 X 45 | 0.3  - old logo*/}
+      {/* 2447 X 626 | 0.26  - new logo*/}
+      <img src={footerLogo} width={231} height={60} alt="Rutgers Logo" />
+    </a>
+  </div>
+)
 
 
+const FooterDisclaimer = () => (
+  <section className={styles.disclaimer}>
+    <p>Rutgers is an equal access/equal opportunity institution. Individuals with disabilities are encouraged to direct suggestions, comments, or complaints concerning any accessibility issues with Rutgers websites to&nbsp;<a href="mailto:accessibility@rutgers.edu">accessibility@rutgers.edu</a>&nbsp;or complete the&nbsp;<a href="https://it.rutgers.edu/it-accessibility-initiative/barrierform/" target="_blank" rel="noreferrer">Report Accessibility Barrier / Provide Feedback</a>&nbsp;form.</p>
 
+    <p>
+      {/* <a href="/copyright-information" target="_blank" rel="noreferrer"> */}
+      Copyright ©{new Date().getFullYear()}
+      {/* </a> */}
+      , Rutgers, The State University of New Jersey. All rights reserved.&nbsp;<br /><a href="mailto:ora-it@njaes.rutgers.edu">Contact webmaster</a></p>
+  </section>
+)
