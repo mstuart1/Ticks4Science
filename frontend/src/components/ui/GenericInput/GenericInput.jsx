bab911d6b9105ca612bb2cf4b6419fbe39cf9978
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-20 11:54:15 -0500

    define accept so it can handle images #134

--- frontend/src/components/ui/GenericInput/GenericInput.jsx
+++ frontend/src/components/ui/GenericInput/GenericInput.jsx
@@ -1,21 +1,22 @@
 import styles from './input.module.css'
 
-const GenericInput = ({label, idString, required, placeholder, type='text'}) => {
-  
+const GenericInput = ({ label, idString, required, placeholder, type = 'text', accept = "image/*" }) => {
+
     return (
-      <div
-          className={styles.inputContainer}
-      >
-          <label className={styles.label} htmlFor={idString}>{label}</label>
-          <input
-              className={styles.input}
-              type={type}
-              name={idString}
-              id={idString}
-              required={required}
-              placeholder={placeholder}
-          />
-      </div>
-  )
+        <div
+            className={styles.inputContainer}
+        >
+            <label className={styles.label} htmlFor={idString}>{label}</label>
+            <input
+                className={styles.input}
+                type={type}
+                accept={accept}
+                name={idString}
+                id={idString}
+                required={required}
+                placeholder={placeholder}
+            />
+        </div>
+    )
 }
 export default GenericInput
