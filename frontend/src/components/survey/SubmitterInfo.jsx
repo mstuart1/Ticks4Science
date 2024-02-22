8c355c10dab919af5cbfe9df5d517a87e6a351cb
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-02-22 10:14:47 -0500

    #134 change submitter style

--- frontend/src/components/survey/SubmitterInfo.jsx
+++ frontend/src/components/survey/SubmitterInfo.jsx
@@ -5,9 +5,17 @@ const SubmitterInfo = () => {
         <div className={styles.formSection}>
             <h4 className={styles.sectionTitle}>Submitter Information</h4>
             <div className={styles.responsiveDiv}>
-                <GenericInput label="Municipality *" idString="userMuni" required={true} placeholder="Where you live" />
-                <GenericInput label="Zip Code *" idString="userZip" required={true} placeholder="Where you live" />
-                <GenericInput label="Would you like to receive email notifications about your tick(s)?" placeholder={'email'} idString="email" required={false}  type='email' />
+                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-evenly' }}>
+                    {/* <div style={{ display: 'flex', flexDirection: 'column' }}> */}
+                        <GenericInput label="Municipality *" idString="userMuni" required={true} placeholder="Where you live" />
+                    {/* </div> */}
+                    {/* <div style={{ display: 'flex', flexDirection: 'column' }}> */}
+                        <GenericInput label="Zip Code *" idString="userZip" required={true} placeholder="Where you live" />
+                    {/* </div> */}
+                </div>
+                        <GenericInput label="Would you like to receive email notifications about your tick(s)?" placeholder={'email'} idString="email" required={false} type='email' />
+               
+               
             </div>
         </div>)
 }
