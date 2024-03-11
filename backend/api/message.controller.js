3ecf5234404da53e809ecdaf7a597b3f7c0d6fe5
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-03-11 12:00:45 -0400

    #141 send email when a message is sent from admin

--- backend/api/message.controller.js
+++ backend/api/message.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const Message = db.message;
+const Submission = db.submission;
 const { mailUser } = require("./mailHelper");
 
 // create message
@@ -8,19 +9,20 @@ exports.createMessage = async (req, res, next) => {
     try {
         let createdRecord;
         let incomingInfo = req.body;
+        
         await db.sequelize
             .transaction(async (t) => {
                 createdRecord = await Message.create(incomingInfo, { transaction: t })
             });
-            // todo #141 find out if related submission has a citizenId, find out if sender of message is user or admin, send email if citizenId and admin
-        // if (updatedTick.citizenId) {
-        //     if (data.photoId || data.specimenId) {
-        //         console.log('emailing user about change')
-        //         let subject = `NJTicks4Science Tick Update for tick id # ${updatedTick.id}`
-        //         let message = `An update has been made to information regarding tick # ${updatedTick.id}.  Check your tick's progress by clicking this link: <a href={"https://ticks.rutgers.edu/progress/${updatedTick.id}"}>https://ticks.rutgers.edu/progress/${updatedTick.id}</a>`
-        //         mailUser(updatedTick.citizen.email, subject, message)
-        //     }
-        // } 
+            if (incomingInfo.role === 'admin') {
+                let foundSub = await Submission.findByPk(incomingInfo.submissionId)
+                // console.log(JSON.stringify(foundSub, null, 1))
+                if (foundSub.citizenId){
+                    let subject = `NJTicks4Science - New Message`
+                    let message = `You have been sent a message regarding your submission. ${incomingInfo.message} Click this link to view your submission: <a href={"https://ticks.rutgers.edu/progress/${foundSub.id}"}>https://ticks.rutgers.edu/progress/${foundSub.id}</a>`
+                    mailUser(foundSub.citizen.email, subject, message)
+                }
+            }
         return res.json({ data: createdRecord })
     } catch (err) {
         console.log(err.message)
