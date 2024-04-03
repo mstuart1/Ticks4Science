4411df43cbd82f577a74eaca24104a9c0d2488df
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-04-03 10:39:24 -0400

    #141 style example image

--- frontend/src/components/survey/MultipleExample.jsx
+++ frontend/src/components/survey/MultipleExample.jsx
@@ -1,8 +1,13 @@
 import multipleExample from '../../images/multipleExample.png'
+import styles from './Survey.module.css'
+
+
 const MultipleExample = () => {
+  
   return (
-    <div>
-        <img src={multipleExample} alt='example of multiple ticks attached to a sheet of paper'/>
+    <div className={styles.multiDiv}>
+        <img className={styles.multiImg} src={multipleExample} alt='example of multiple ticks attached to a sheet of paper'/>
+        <p className={styles.multiMsg}>This window opened in a new tab.  Check your browser for the tab with your current survey data.</p>
     </div>
   )
 }
