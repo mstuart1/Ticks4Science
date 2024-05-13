2a83ec668cf8d8b1d69e12c4265f21dc1bd6dc9f
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-05-13 10:13:56 -0400

    #144 convert how id is used in input

--- frontend/src/components/survey/Survey.jsx
+++ frontend/src/components/survey/Survey.jsx
@@ -12,27 +12,42 @@ import getFormValues from "./getFormValues";
 import extractFromObj from "../../tools/extractFromObj";
 import renameKeys from "../../tools/renameKeys";
 import OutlineFloatButton from '../ui/outlineFloatButton/OutlineFloatButton'
+import GenericInput from "../ui/GenericInput";
 // import SurveyForm from "./SurveyForm";
 
 const Survey = () => {
   const navigate = useNavigate();
 
+  let initialTick = { key: 0, userMuni: '', userZip: '', email: '', dateTickFound: '', foundOn: '', foundOnOther: '', tickAttached: '', animal: '', dateRemoved: '', tickLocation: '', locationDesc: '', locationDescOther: '', tickMuni: '', tickCounty: '', tickZip: '', activities: '', personBitten: '', submitterBitten: '', bittenMuni: '', bittenZip: '', bittenTraveledDom: '', bittenTraveledIntl: '', travelInfo: '', photoFrontUrl: '', photoBackUrl: '', additionalInfo: '', blitzLoc: '', blitzZip: '', sampler: '', timeStart: '', timeEnd: '', timeBlock: '', distance: '', weather: '', weatherOther: '', blitzPhotos: '', blitzAddInfo: '' }
+
+
   const [inProgress, setInProgress] = useState(false);
   const [error, setError] = useState('');
   const [numTicks, setNumTicks] = useState(1);
-  const [formValues, setFormValues] = useState({});
+  const [ticks, setTicks] = useState([initialTick])
 
+  const handleFormData = ({target: {name, value}}) => {
+    let id = parseInt(name.split(',')[1]);
+    name = name.split(',')[0] ;
+    setTicks((prev) => prev.map((tick) => parseInt(tick.key) === parseInt(id) ? { ...tick, [name]: value } : tick))
+  }
+
+  const handleNumTicks = ({ target: { value } }) => {
+    setNumTicks(value)
+    setTicks((prev) => [...prev, { ...initialTick, key: parseInt(value-1) }])
+
+  }
   const handleSubmit = async (evt, index) => {
 
     try {
       // setInProgress(true);
       evt.preventDefault();
       console.log('index', index)
-      const {  data } = getFormValues(evt.currentTarget);
+      const { data } = getFormValues(evt.currentTarget);
       console.log('data', data[`tick1.imageFront`].type)
       // let id = 0
       // let idArray = []
-      // for (let i = 1; i <= numTicks; i++) {
+      // for (let i = 0; i <= numTicks; i++) {
       //   let allTicksKeys = Object.keys(data).filter((key) => !key.includes(`tick`));
       //   let oneTickKeys = Object.keys(data).filter((key) => key.includes(`tick${i}`));
       //   const allTickData = extractFromObj(data, allTicksKeys)
@@ -75,28 +90,28 @@ const Survey = () => {
       // // alert(JSON.stringify(response.data))
 
     } catch (err) {
-     setError(true)
+      setError(true)
     }
   };
 
   const generateForm = () => {
     let form = [];
-    for (let i = 1; i <= numTicks; i++) {
-      // let tickNum = i + 1;
+    for (let i = 0; i <= numTicks-1; i++) {
+      let currentTick = ticks.find((tick) => tick.key === i)
       form.push(
-        
+
         <details open={numTicks === 1} className={styles.details} key={i}>
-          <summary className={styles.summary}>Tick {i}<span style={{color: '#c00300', marginLeft: '0.5rem'}}>ID# will generate when you submit the form</span> </summary>
-         {i > 1 && <OutlineFloatButton colors={{ text: '#00626d', shadow: '#00626d', bg: '#ebb600' }} handleClick={(evt) => handleSubmit(evt, i)} text='Use previous tick info'/>}
-           <form onSubmit={handleSubmit} className={styles.form}>
-        <SubmitterInfo data={formValues} handleChange={setFormValues} />
-       {numTicks > 1 ?  <p style={{ padding: '0 10rem' }}>Click each tick to answer questions.</p> : null}
-          <TickAttached id={i} />
-          <TickLocation id={i} />
+          <summary className={styles.summary}>Tick {i+1}<span style={{ color: '#c00300', marginLeft: '0.5rem' }}>ID# will generate when you submit the form</span> </summary>
+          {i > 1 && <OutlineFloatButton colors={{ text: '#00626d', shadow: '#00626d', bg: '#ebb600' }} handleClick={(evt) => handleSubmit(evt, i)} text='Use previous tick info' />}
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <SubmitterInfo id={i} data={currentTick} handleChange={handleFormData} />
+            {numTicks > 1 ? <p style={{ padding: '0 10rem' }}>Click each tick to answer questions.</p> : null}
+               <TickAttached id={i} data={currentTick} handleData={handleFormData} />
+             <TickLocation id={i} handleData={handleFormData} data={currentTick}/>
           <PhotoSection id={i} />
             <div className={styles.btnDiv}>
               <button className={styles.submitBtn}>
-                Submit
+                <span className={styles.submitSpan}>Save this tick</span>
               </button>
 
               <BasicPage.LinkButton.LinkSpec to={"/steps"}>
@@ -107,9 +122,9 @@ const Survey = () => {
                 </BasicPage.HoverCard>
               </BasicPage.LinkButton.LinkSpec>
             </div>
-          </form> 
+          </form>
         </details>
-         
+
       );
     }
     return form;
@@ -117,12 +132,15 @@ const Survey = () => {
 
   let formElems = generateForm();
 
+  console.log('ticks', ticks)
+
   if (error.length) {
-    return (<div className={styles.errorDiv}><h1>{error ??  'There was an error processing your form.'}</h1>
-     <OutlineFloatButton colors={{ text: '#00626d', shadow: '#00626d', bg: '#00626d' }} handleClick={() => navigate(0)} text='Return to Survey'
-          />
+    return (<div className={styles.errorDiv}><h1>{error ?? 'There was an error processing your form.'}</h1>
+      <OutlineFloatButton colors={{ text: '#00626d', shadow: '#00626d', bg: '#00626d' }} handleClick={() => navigate(0)} text='Return to Survey'
+      />
     </div>)
   }
+
   if (inProgress) {
     return (<div style={{ width: '100vw', height: '100vh' }}><h1>Submitting form...</h1></div>)
   }
@@ -137,19 +155,19 @@ const Survey = () => {
         <p>Answer the questions below to the best of your ability; if you do not remember the exact dates or locations give your best estimate.</p>
         <div className={styles.formSection}>
           How many ticks do you intend to mail in?
-          <input className={styles.input} type="number" min="1" onChange={(evt) => setNumTicks(evt.target.value)} value={numTicks} />
+          <input className={styles.input} type="number" min="1" onChange={handleNumTicks} value={numTicks} />
           {numTicks > 1 ? (<p> <span className={styles.specialText}>*Note: Before mailing ticks in, they must be labeled with the corresponding number you assign them below (i.e Tick 1, Tick 2, etc). You must also label somewhere in/on the envelope the tick ID number you receive after submitting this form. See example of acceptable submissions <Link to='/multipleExample' target="_blank">here</Link>. </span> </p>) : null}
         </div>
       </div>
 
-        {formElems}
-        
+      {formElems}
 
 
-  {numTicks > 1 ? <p style={{padding: '0 10rem'}}>If the form will not submit, click each tick (for example "Tick 1") to open the question section.  If the section is open when submitting, it will display which question is preventing the form from submitting.</p> : null}
+
+      {/* {numTicks > 1 ? <p style={{padding: '0 10rem'}}>If the form will not submit, click each tick (for example "Tick 1") to open the question section.  If the section is open when submitting, it will display which question is preventing the form from submitting.</p> : null} */}
 
 
-      
+
     </div>
   );
 };
