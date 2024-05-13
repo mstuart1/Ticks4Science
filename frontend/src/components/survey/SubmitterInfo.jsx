dc81302e3bcaf2653da473c5a1f328a880304d20
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-05-13 10:13:38 -0400

    #144 convert how id is used in input

--- frontend/src/components/survey/SubmitterInfo.jsx
+++ frontend/src/components/survey/SubmitterInfo.jsx
@@ -1,19 +1,26 @@
 import GenericInput from '../ui/GenericInput'
 import styles from './Survey.module.css'
-const SubmitterInfo = () => {
+const SubmitterInfo = ({id, data={}, handleChange }) => {
     return (
         <div className={styles.formSection}>
             <h4 className={styles.sectionTitle}>Submitter Information</h4>
             <div className={styles.responsiveDiv}>
                 <div style={{ display: 'flex', width: '100%', justifyContent: 'space-evenly' }}>
-                    {/* <div style={{ display: 'flex', flexDirection: 'column' }}> */}
-                        <GenericInput label="City *" idString="userMuni" required={true} placeholder="Where you live" />
-                    {/* </div> */}
-                    {/* <div style={{ display: 'flex', flexDirection: 'column' }}> */}
-                        <GenericInput label="Zip Code *" idString="userZip" required={true} placeholder="Where you live" />
-                    {/* </div> */}
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <GenericInput label="City *" name={`userMuni,${id}`} required={true} placeholder="Where you live"
+                         value={data.userMuni || null}
+                          handleChange={handleChange}/>
+                        
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <GenericInput label="Zip Code *" name={`userZip,${id}`} required={true} placeholder="Where you live" 
+                        value={data.userZip || null}
+                         handleChange={handleChange} />
+                    </div>
                 </div>
-                        <GenericInput label="Would you like to receive email notifications about your tick(s)?" placeholder={'email'} idString="email" required={false} type='email' />
+                        <GenericInput label="Would you like to receive email notifications about your tick(s)?" placeholder={'email'} name={`email,${id}`} required={false} type='email'  
+                        value={data.email || null} 
+                        handleChange={handleChange} />
                
                
             </div>
