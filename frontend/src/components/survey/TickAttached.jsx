00bf13813170d9c57501357a97462218377f85bb
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-05-13 09:36:52 -0400

    remove tick${id} reference

--- frontend/src/components/survey/TickAttached.jsx
+++ frontend/src/components/survey/TickAttached.jsx
@@ -5,53 +5,48 @@ import GenericTextarea from '../ui/GenericTextarea'
 import { yesNoArray, foundArray } from './surveyFormData'
 import { useState } from 'react'
 
-const TickAttached = ({id}) => {
+const TickAttached = ({id, data, handleData}) => {
 
-    const [input, setInput] = useState({})
-    const handleChange = ({ target: { value, name } }) => {
-        setInput((prevState) => ({ ...prevState, [name]: value }))
-    }
-
-    // console.log('tickAttached.input', input)
     return (
         <div className={styles.formSection}>
             <h4 className={styles.sectionTitle}>Tick Embedded Information</h4>
             <div className={styles.responsiveDiv}>
-                <GenericRadio label={`Indicate on who/what tick(s) were found *`} idString={`tick${id}.foundOn`} valueArray={foundArray} state={input} handleChange={handleChange} />
-                {input[`tick${id}.foundOn`] === "Other" && (
-                    <GenericInput idString={`tick${id}.foundOnOther`} required={input[`tick${id}.foundOn`] === "Other"} placeholder="Describe the object where you found the tick" />
+                <GenericRadio label={`Indicate on who/what tick(s) were found *`} name={'foundOn'} id={id}  valueArray={foundArray} state={data.foundOn} handleChange={handleData} />
+                {data.foundOn === "Other" && (
+                    <GenericInput id={id} name={`foundOnOther`} required={data.foundOn === "Other"} placeholder="Describe the object where you found the tick" value={data.foundOnOther} handleChange={handleData} />
                 )}
-                {input[`tick${id}.foundOn`] === "Animal" && (
-                    <GenericInput idString={`tick${id}.animal`} required={input[`tick${id}.foundOn`] === "Animal"} placeholder="Specify Animal" />
+                {data.foundOn === "Animal" && (
+                    <GenericInput id={id} name={`animal`} required={data.foundOn === "Animal"} placeholder="Specify Animal" value={data.animal} handleChange={handleData} />
                 )}
 
-                {(input[`tick${id}.foundOn`] === "Person" || input[`tick${id}.foundOn`] === "Animal") && (
+                {(data.foundOn === "Person" || data.foundOn === "Animal") && (
                     <>
-                        <GenericRadio label={`If tick(s) were found on person/animal, was it embedded *?`} idString={`tick${id}.tickAttached`} valueArray={yesNoArray} state={input} handleChange={handleChange} />
+                        <GenericRadio label={`If tick(s) were found on person/animal, was it embedded *?`} id={id} name={`tickAttached`} valueArray={yesNoArray} state={data} handleChange={handleData} />
                         
 
 
-                        {input[`tick${id}.tickAttached`]?.toLowerCase() === "yes" && (
+                        {data.tickAttached?.toLowerCase() === "yes" && (
                             <>
 
-                                <GenericInput label="Date Removed *" idString={`tick${id}.dateRemoved`} type="date" required={true} />
+                                <GenericInput label="Date Removed *" id={id} name={`dateRemoved`} type="date" required={true} value={data.dateRemoved} handleChange={handleData} />
 
 
-                                {input[`tick${id}.foundOn`] === "Person" && (
+                                {data.foundOn === "Person" && (
                                     <>
-                                        <GenericRadio label={`Was the person bitten by the tick? *`} idString={`tick${id}.submitterBitten`} valueArray={yesNoArray} state={input} handleChange={handleChange} />
+                                        <GenericRadio label={`Was the person bitten by the tick? *`} id={id} name={`submitterBitten`} valueArray={yesNoArray} state={data} handleChange={handleData} />
 
-                                        {input[`tick${id}.submitterBitten`] === "yes" && (
+                                        {data.submitterBitten === "yes" && (
                                             <>
-                                                <GenericInput label="City of bitten person *" idString={`tick${id}.bittenMuni`} required={true} placeholder="Where bitten person lives" />
-                                                <GenericInput label="ZIP Code of bitten person *" idString="bittenZip" required={true} placeholder="Where bitten person lives" />
+                                                <GenericInput label="City of bitten person *" id={id} name={`.bittenMuni`} required={true} placeholder="Where bitten person lives"  value={data.bittenMuni} handleChange={handleData}/>
+                                                <GenericInput label="ZIP Code of bitten person *" id={id} name="bittenZip" required={true} placeholder="Where bitten person lives" value={data.bittenZip} handleChange={handleData}/>
                                             </>
                                         )}
 
-                                        <GenericRadio label={`Has the bitten person traveled outside of New Jersey within the past 10 days? *`} idString={`tick${id}.bittenTraveled`} valueArray={yesNoArray} state={input} handleChange={handleChange} />
+                                        <GenericRadio label={`Has the bitten person traveled outside of New Jersey within the past 10 days? *`} id={id} name={`bittenTraveled`} valueArray={yesNoArray} state={data} handleChange={handleData} />
 
-                                        {input[`tick${id}.bittenTraveled`] === "yes" && (
-                                            <GenericTextarea label="Where did the bitten person travel?" idString={`tick${id}.travelInfo`} />
+                                        {data.bittenTraveled === "yes" && (
+                                            <GenericTextarea label="Where did the bitten person travel?" id={id} name={`travelInfo`}
+                                            value={data.travelInfo} handleChange={handleData} />
 
                                         )}
                                     </>
