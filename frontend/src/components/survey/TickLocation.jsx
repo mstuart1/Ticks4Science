13870fbbc5756fd1c6d9794d4469112cd60716b7
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-05-13 14:35:56 -0400

    update inputs to handle name and id #144

--- frontend/src/components/survey/TickLocation.jsx
+++ frontend/src/components/survey/TickLocation.jsx
@@ -2,38 +2,44 @@ import styles from './Survey.module.css'
 import GenericInput from '../ui/GenericInput'
 import GenericTextarea from '../ui/GenericTextarea'
 import GenericRadio from '../ui/GenericRadio'
-import { useState } from 'react'
 import { locationOptions } from './surveyFormData'
 
-const TickLocation = ({ id }) => {
-
-    const [input, setInput] = useState({})
-    const handleChange = ({ target: { value, name } }) => {
-        setInput((prevState) => ({ ...prevState, [name]: value }))
-    }
+const TickLocation = ({ id, data = {}, handleData }) => {
 
     return (
-        <div className={styles.tickSection}>
+        <div className={styles.formSection}>
 
             <h4 className={styles.sectionTitle}>Tick Location Information</h4>
             <div className={styles.responsiveDiv}>
                 <div className={styles.tickSubSection}>
                     <div className={styles.tickSubSection}>
-                        <GenericInput label="Date tick was found *" idString={`tick${id}.dateTickFound`} required={true} type='date' />
-                        <GenericInput label="City where tick was found *" idString={`tick${id}.tickMuni`} required={true} type='text' />
+                        <GenericInput value={data.dateTickFound} handleChange={handleData} label="Date tick was found *"  name={`dateTickFound,${id}`} required={true} type='date' />
+                        <GenericInput 
+                        value={data.tickMuni} handleChange={handleData} 
+                        label="City where tick was found *"  
+                        name={`tickMuni,${id}`} 
+                        required={true} type='text' />
+                        
                     </div>
                     <div className={styles.tickSubSection}>
-                        <GenericInput label="County where tick was found *" idString={`tick${id}.tickCounty`} required={true} type='text' />
-                        <GenericInput label="ZIP code where tick was found *" idString={`tick${id}.tickZip`} required={true} type='text' />
+                        <GenericInput value={data.tickCounty} handleChange={handleData} label="County where tick was found *"  name={`tickCounty,${id}`} required={true} type='text' />
+                        <GenericInput value={data.tickZip} handleChange={handleData} label="ZIP code where tick was found *"  name={`tickZip,${id}`} required={true} type='text' />
                     </div>
                 </div>
-                <GenericRadio label="Descriptor of Location*" idString={`tick${id}.locationDesc`} valueArray={locationOptions} state={input} handleChange={handleChange} />
-                {console.log('input', input[`tick${id}.locationDesc`])}
-                {input[`tick${id}.locationDesc`] === "Other" && (
-                    <GenericInput idString={`tick${id}.locationDescOther`} required={input[`tick${id}.locationDesc`] === "Other"} placeholder="Describe the location" />
+                <GenericRadio
+                    label="Descriptor of Location*"
+                    name={`locationDesc,${id}`}
+                    valueArray={locationOptions}
+                    state={data.locationDesc}
+                    handleChange={handleData}
+                />
+                
+
+                {data.locationDesc === "Other" && (
+                    <GenericInput value={data.locationDescOther} handleChange={handleData} name={`locationDescOther,${id}`} required={data.locationDesc === "Other"} placeholder="Describe the location" />
 
                 )}
-                <GenericTextarea label="Describe activity/activities when tick(s) were acquired:" idString={`tick${id}.activities`} />
+                <GenericTextarea label="Describe activity/activities when tick(s) were acquired:"  name={`activities,${id}`} value={data.activities} onChange={handleData} />
             </div>
 
         </div>
