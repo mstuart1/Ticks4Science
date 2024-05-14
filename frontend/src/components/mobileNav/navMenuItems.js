50171b8e9e8e7bd05a64b64e4c22984989e99d67
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-05-14 15:36:26 -0400

    add education to nav

--- frontend/src/components/mobileNav/navMenuItems.js
+++ frontend/src/components/mobileNav/navMenuItems.js
@@ -1,11 +1,12 @@
-import { FaEye, FaHome, FaListOl, FaSpider, FaStroopwafel } from "react-icons/fa"
+import { FaEye, FaHome, FaListOl, FaSpider, FaStroopwafel, FaRegBookmark } from "react-icons/fa"
 
 const navMenuItems = [
     {id: 1, title: 'Home', path: '/', icon: <FaHome/>},
     {id: 2, title: 'Join the Project', path: '/steps', icon: <FaListOl/>},
     {id: 3, title: 'Ticks of NJ', path: '/ticks', icon: <FaSpider/>},
     {id: 4, title: `Prevent Tick Bites`, path: '/checkMenu', icon: <FaEye/>},
-    {id: 5, title: `Tick Blitz`, path: '/tickBlitz', icon: <FaStroopwafel/>},
+    // {id: 5, title: `Tick Blitz`, path: '/tickBlitz', icon: <FaStroopwafel/>},
+    {id: 6, title: `Education`, path: '/education', icon: <FaRegBookmark/>},
 ]
 
 export default navMenuItems
