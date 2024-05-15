3bbe807227fd6289a55383fb201b6fab2d7f4780
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-05-15 08:10:25 -0400

    add foreign key to association #146

--- backend/models/users.js
+++ backend/models/users.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      users.hasMany(models.message, {as: 'admin'})
-      users.hasMany(models.message, {as: 'deletedBy'})
+      users.hasMany(models.message, {as: 'admin', foreignKey: 'adminId'})
+      users.hasMany(models.message, {as: 'deletedBy', foreignKey: 'deletedById'})
     }
   }
   users.init({
