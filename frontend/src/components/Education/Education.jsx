82fdb4c03773d47e69eeb858da62031cd485c4f5
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-06-10 12:08:03 -0400

    update education images

--- frontend/src/components/Education/Education.jsx
+++ frontend/src/components/Education/Education.jsx
@@ -15,19 +15,27 @@ const Education = () => {
   return (
     <div className={styles.pageCont}>
     <Link className={styles.card} to={tickMisconPdf} target="_blank"><div>
-        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={200} height={300} src={tickMisconImg}/>
+        <img alt={'infographic about ticks in new jersey'} className={styles.img} 
+        width={300}
+         src={tickMisconImg}/>
         <p className={styles.title}>NJT4S! Brochure (Front)</p>
         </div></Link>
     <Link className={styles.card} to={njTicksPdf} target="_blank"><div>
-        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={200} height={300} src={njTicksImg}/>
+        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={300} 
+        // height={300}
+         src={njTicksImg}/>
         <p className={styles.title}>NJT4S! Brochure (Back)</p>
         </div></Link>
     <Link className={styles.card} to={freeTestPdf} target="_blank"><div>
-        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={200} height={300} src={freeTestImg}/>
+        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={300}
+        //  height={300} 
+         src={freeTestImg}/>
         <p className={styles.title}>NJT4S! General Flyer</p>
         </div></Link>
     <Link className={styles.card} to={seeTickPdf} target="_blank"><div>
-        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={200} height={300} src={seeTickImg}/>
+        <img alt={'infographic about ticks in new jersey'} className={styles.img} width={300}
+        //  height={300} 
+         src={seeTickImg}/>
         <p className={styles.title}>NJT4S! How to Send in a Tick Flyer</p>
         </div></Link>
     </div>
