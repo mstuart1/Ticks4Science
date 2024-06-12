96a3e3d5dd0227cdd37977493704c38d791fcf11
Michelle Stuart <michelle.stuart@rutgers.edu>
2024-06-12 12:11:47 -0400

    update handleNumTicks to handle changes on mobile screen

--- frontend/src/components/survey/Survey.jsx
+++ frontend/src/components/survey/Survey.jsx
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import SubmissionDataService from "../../services/submission";
 import styles from './Survey.module.css'
@@ -29,10 +29,10 @@ const Survey = () => {
       console.log('checking tick', tick.key + 1)
       let required = extractFromObj(tick, requiredFields)
       console.log('tick image', tick.imageFront)
-      console.log('emptyFields', Object.entries(required).filter(item => (item[1] === undefined || item[1] === null || item[1].length === 0  )))
-      
+      console.log('emptyFields', Object.entries(required).filter(item => (item[1] === undefined || item[1] === null || item[1].length === 0)))
+
       // console.log('checking empty fields', emptyFields)
-      let emptyFields = Object.entries(required).filter(item => (item[1] === undefined || item[1] === null || item[1].length === 0 ))
+      let emptyFields = Object.entries(required).filter(item => (item[1] === undefined || item[1] === null || item[1].length === 0))
       if (emptyFields.length > 0) {
         alert(`Please fill out all of the fields with an asterisk for tick ${tick.key + 1}`)
         return formIsValid = false;
@@ -97,15 +97,25 @@ const Survey = () => {
   }
 
   const handleNumTicks = ({ target: { value } }) => {
+    //!! on some mobile, the up down arrows disappear from the numTicks input, and if someone deletes the previous number to set a new number, it sets the numTicks to zero and sets ticks to an empty array
     setNumTicks(value)
     if (value < ticks.length) {
       let freshTicks = ticks.filter(tick => tick.key < value)
-    setTicks(freshTicks)
+      setTicks(freshTicks)
 
-  } else {
-      setTicks((prev) => [...prev, { ...initialTick, key: parseInt(value - 1) }])
+    } else {
+      let freshTicks = []
+      if (ticks.length === 0) {
+        for (let step = 0; step < value; step++) {
+           freshTicks.push({ ...initialTick, key: step })
+        }
+        
+      } else {
+         freshTicks = [...ticks, { ...initialTick, key: parseInt(value - 1) }]
+      }
+      setTicks(freshTicks)
+    }
   }
-}
   const handleSubmit = async (evt) => {
 
     try {
@@ -119,7 +129,7 @@ const Survey = () => {
       let response = await SubmissionDataService.submitBulkForm(ticks);
       let ids = response.data.data.map(item => item.id)
       // console.log('ids', ids)
-      for await (let id of ids){
+      for await (let id of ids) {
         let index = ids.indexOf(id)
         let tick = ticks.find(tick => tick.key === index)
         let photoFormData = new FormData();
@@ -131,7 +141,7 @@ const Survey = () => {
           },
         };
         let response2 = await SubmissionDataService.submitImage(id, photoFormData, config).catch(err => { console.log(err) });
-        // console.log('response', response2)
+        console.log('response2', response2)
 
       }
 
@@ -160,54 +170,57 @@ const Survey = () => {
     return (<div style={{ width: '100vw', height: '100vh' }}><h1>Submitting form...</h1></div>)
   }
 
+  console.log('ticks', ticks)
+  console.log('Numticks', numTicks)
+
   return (
     <div className={styles.text}>
       <h2 className={styles.title}>Tick Submission Form</h2>
       <div className={styles.form}>
         <p> Answer the questions below to the best of your ability; if you do not remember the exact dates or locations give your best estimate.</p>
-        <p className={styles.specialText}> 
-        
-            *Note: Tick(s) must be in a sealed baggie or taped down with their tick ID number(s) before mailing in. DO NOT wrap ticks in tissues, paper towels, etc before mailing in. Wrapping ticks in tissue/paper towels make it more likely that the tick will get lost upon opening. Therefore, if you choose to send your tick in a plastic baggie do not include anything other than the tick inside the baggie. 
-        </p><p className={styles.specialText}> You will get a tick ID number after submitting this form which must be included somewhere in/on your envelope. If you are submitting multiple ticks, you will be assigned multiple ID numbers and must label each tick with their corresponding number.  See examples of acceptable submissions <Link to='/multipleExample' target="_blank">here</Link>. 
-          </p>
-      <div className={styles.checkboxDiv}>
-        <label>
-      Click the box to acknowledge the message in red
-      </label>
-        <input className={styles.input} type="checkbox" onChange={(evt) => setShowSurvey(evt.target.checked)}/>
-      </div>
+        <p className={styles.specialText}>
+
+          *Note: Tick(s) must be in a sealed baggie or taped down with their tick ID number(s) before mailing in. DO NOT wrap ticks in tissues, paper towels, etc before mailing in. Wrapping ticks in tissue/paper towels make it more likely that the tick will get lost upon opening. Therefore, if you choose to send your tick in a plastic baggie do not include anything other than the tick inside the baggie.
+        </p><p className={styles.specialText}> You will get a tick ID number after submitting this form which must be included somewhere in/on your envelope. If you are submitting multiple ticks, you will be assigned multiple ID numbers and must label each tick with their corresponding number.  See examples of acceptable submissions <Link to='/multipleExample' target="_blank">here</Link>.
+        </p>
+        <div className={styles.checkboxDiv}>
+          <label>
+            Click the box to acknowledge the message in red
+          </label>
+          <input className={styles.input} type="checkbox" onChange={(evt) => setShowSurvey(evt.target.checked)} />
+        </div>
       </div>
       {showSurvey && (
         <>
-         <div className={styles.formSection}>
-          How many ticks do you intend to mail in?
-          <input className={styles.input} type="number" min="1" onChange={handleNumTicks} value={numTicks} />
-          
-        </div>
-    
-      <form>
-        {ticks.map((currentTick, i) => {
-          return (
-            <details open={numTicks === 1} className={styles.details} key={i}>
-            <summary className={styles.summary}>Tick {i + 1}<span style={{ color: '#c00300', marginLeft: '0.5rem' }}>ID# will generate when you submit the form</span> </summary>
-            {i > 0 && <OutlineFloatButton colors={{ text: '#00626d', shadow: '#00626d', bg: '#ebb600' }} handleClick={() => handleCopyTick(i)} text='Use previous tick info' />}
-            <SubmitterInfo id={i} data={currentTick} handleChange={handleFormData} />
-            {numTicks > 1 ? <p style={{ padding: '0 10rem' }}>Click each tick to answer questions.</p> : null}
-            <TickAttached id={i} data={currentTick} handleData={handleFormData} />
-            <TickLocation id={i} handleData={handleFormData} data={currentTick} />
-            <PhotoSection id={i} handleData={handlePhoto} />
-          </details>
-          )
-        }
+          <div className={styles.formSection}>
+            How many ticks do you intend to mail in?
+            <input className={styles.input} type="number" min="1" onChange={handleNumTicks} value={numTicks} />
+
+          </div>
+
+          <form>
+            {ticks.map((currentTick, i) => {
+              return (
+                <details open={numTicks === 1} className={styles.details} key={i}>
+                  <summary className={styles.summary}>Tick {i + 1}<span style={{ color: '#c00300', marginLeft: '0.5rem' }}>ID# will generate when you submit the form</span> </summary>
+                  {i > 0 && <OutlineFloatButton colors={{ text: '#00626d', shadow: '#00626d', bg: '#ebb600' }} handleClick={() => handleCopyTick(i)} text='Use previous tick info' />}
+                  <SubmitterInfo id={i} data={currentTick} handleChange={handleFormData} />
+                  {numTicks > 1 ? <p style={{ padding: '0 10rem' }}>Click each tick to answer questions.</p> : null}
+                  <TickAttached id={i} data={currentTick} handleData={handleFormData} />
+                  <TickLocation id={i} handleData={handleFormData} data={currentTick} />
+                  <PhotoSection id={i} handleData={handlePhoto} />
+                </details>
+              )
+            }
+            )}
+          </form>
+          <div className={styles.btnDiv}>
+            <OutlineFloatButton width='200px' colors={{ text: '#00626d', shadow: '#00626d', bg: '#00626d' }} handleClick={handleSubmit} text='Submit Form' />
+            <BorderlessFloatButton width='200px' colors={{ text: '#00626d', shadow: '#00626d' }} handleClick={() => navigate('/steps')} text='Cancel' />
+          </div>
+        </>
       )}
-      </form>
-      <div className={styles.btnDiv}>
-        <OutlineFloatButton width='200px' colors={{ text: '#00626d', shadow: '#00626d', bg: '#00626d' }} handleClick={handleSubmit} text='Submit Form' />
-        <BorderlessFloatButton width='200px' colors={{ text: '#00626d', shadow: '#00626d' }} handleClick={() => navigate('/steps')} text='Cancel' />
-      </div>
-      </>
-      )}
-       
+
 
     </div>
   );
